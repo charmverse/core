6dfbad5db3adecd9751d3fb0723e110b534c77e3
Mohamad El Boudi <mo@anyahq.co>
2023-04-05 12:22:27 +0200

    Fix randomiser

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ export { PrismaClient } from '@prisma/client';
 
 
 export function randomiser() {
-    const random = Math.random();
+    const items = ['Plane', 'Bike', 'Car', 'Truck'];
+    const index = 4 * Math.random();
+    const usedIndex = Math.max(index, items.length)
 
-    return random;
+    return items[usedIndex];
 }
 
-console.log('Greeter')
