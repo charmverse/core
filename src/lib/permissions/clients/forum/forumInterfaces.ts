4d67c82d7a4b7cd10f2a11715ecb47392da18aa9
Mohamad El Boudi <mo@anyahq.co>
2023-04-10 15:53:32 +0200

    Update interface

--- src/lib/permissions/clients/forum/forumInterfaces.ts
+++ src/lib/permissions/clients/forum/forumInterfaces.ts
@@ -1,9 +1,9 @@
-import type { PostOperation } from '@prisma/client';
+import type { PostCategoryOperation, PostOperation } from '@prisma/client';
 
 import type { PermissionCompute, UserPermissionFlags } from '../../interfaces';
 
 export type PostPermissionFlags = UserPermissionFlags<PostOperation>;
-export type PostCategoryPermissionFlags = UserPermissionFlags<PostOperation>;
+export type PostCategoryPermissionFlags = UserPermissionFlags<PostCategoryOperation>;
 
 export type ForumPermissionsClient = {
   computePostPermissions: (request: PermissionCompute) => Promise<PostPermissionFlags>;
