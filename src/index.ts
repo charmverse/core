a6ddaf5d4aff4e1e2ee420d04de29fee5e9516f4
Mohamad El Boudi <mo@anyahq.co>
2023-04-14 13:45:54 +0200

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,3 @@ export * from './lib/user';
 export * from './lib/testing';
 export * from './db';
 export * from './shared';
-
-export function randomiser() {
-  const items = ['Plane', 'Bike', 'Car', 'Truck'];
-  const index = 4 * Math.random();
-  const usedIndex = Math.max(index, items.length);
-
-  return items[usedIndex];
-}
--- src/shared.ts
+++ src/shared.ts
@@ -5,3 +5,12 @@ export * from './lib/utilities';
 export { log } from './lib/log';
 export * from './lib/permissions/clients/interfaces';
 export * from './lib/permissions/interfaces';
+
+// Sample utility function to check imports work
+export function randomiser() {
+  const items = ['Plane', 'Bike', 'Car', 'Truck'];
+  const index = 4 * Math.random();
+  const usedIndex = Math.max(index, items.length);
+
+  return items[usedIndex];
+}
