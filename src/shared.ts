16087bdd0ff1ccf39fe5f201ac672945b2b4c9c8
motechFR <mo@anyahq.co>
2023-04-27 20:07:52 +0200

    Fix circular import

--- src/shared.ts
+++ src/shared.ts
@@ -2,7 +2,7 @@ export * from './lib/errors/index';
 
 export * from './lib/utilities/index';
 
-export { log } from './lib/log';
+export { log } from './lib/log/index';
 export * from './lib/permissions/clients/index';
 export * from './lib/permissions/interfaces';
 
