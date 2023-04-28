877fca391bd3e79dbea64f3e375022a70cdf68a8
motechFR <mo@anyahq.co>
2023-04-28 03:36:22 +0300

    Update permissions client and add watch mode for local dev

--- src/lib/permissions/forums/client/forumPermissionsHttpClient.ts
+++ src/lib/permissions/forums/client/forumPermissionsHttpClient.ts
@@ -26,72 +26,52 @@ export class ForumPermissionsHttpClient extends AbstractPermissionsApiClient imp
   }
 
   getPermissionedCategories(userAndCategories: CategoriesToFilter): Promise<PostCategoryWithPermissions[]> {
-    return fetch(`${this.prefix}/filter-categories`, {
+    return fetch(`${this.prefix}/get-permissioned-categories`, {
       method: 'POST',
-      body: JSON.stringify(userAndCategories),
-      headers: {
-        Authorization: `Bearer ${this.authKey}`
-      }
+      body: JSON.stringify(userAndCategories)
     });
   }
 
   computePostPermissions(request: PermissionCompute): Promise<PostPermissionFlags> {
-    return fetch(`${this.prefix}/compute-post-permissions`, {
-      method: 'POST',
-      body: JSON.stringify(request),
-      headers: {
-        Authorization: `Bearer ${this.authKey}`
-      }
+    return fetch(`${this.prefix}/compute-post-permissions?resourceId=${request.resourceId}&userId=${request.userId}`, {
+      method: 'GET'
     });
   }
 
   computePostCategoryPermissions(request: PermissionCompute): Promise<PostCategoryPermissionFlags> {
-    return fetch(`${this.prefix}/compute-post-category-permissions`, {
-      method: 'POST',
-      body: JSON.stringify(request),
-      headers: {
-        Authorization: `Bearer ${this.authKey}`
+    return fetch(
+      `${this.prefix}/compute-post-category-permissions?resourceId=${request.resourceId}&userId=${request.userId}`,
+      {
+        method: 'GET'
       }
-    });
+    );
   }
 
   assignDefaultPostCategoryPermissions(postCategory: Resource): Promise<void> {
     return fetch(`${this.prefix}/assign-default-post-category-permissions`, {
       method: 'POST',
-      body: JSON.stringify(postCategory),
-      headers: {
-        Authorization: `Bearer ${this.authKey}`
-      }
+      body: JSON.stringify(postCategory)
     });
   }
 
   upsertPostCategoryPermission(request: PostCategoryPermissionAssignment): Promise<AssignedPostCategoryPermission> {
     return fetch(`${this.prefix}/upsert-post-category-permission`, {
       method: 'POST',
-      body: JSON.stringify(request),
-      headers: {
-        Authorization: `Bearer ${this.authKey}`
-      }
+      body: JSON.stringify(request)
     });
   }
 
   deletePostCategoryPermission(request: PermissionToDelete): Promise<void> {
     return fetch(`${this.prefix}/delete-post-category-permission`, {
-      method: 'POST',
-      body: JSON.stringify(request),
-      headers: {
-        Authorization: `Bearer ${this.authKey}`
-      }
+      method: 'DELETE',
+      body: JSON.stringify(request)
     });
   }
 
   mutatePostCategorySearch(request: PostSearchToMutate): Promise<MutatedPostSearch> {
     return fetch(`${this.prefix}/mutate-post-category-search`, {
       method: 'POST',
-      body: JSON.stringify(request),
-      headers: {
-        Authorization: `Bearer ${this.authKey}`
-      }
+      body: JSON.stringify(request)
     });
   }
 }
