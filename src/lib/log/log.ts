eefe4502dce9230011f98f91eb142249f8f90723
Matt Casey <mattwad@gmail.com>
2023-05-04 16:04:08 -0600

    fix global export

--- src/lib/log/log.ts
+++ src/lib/log/log.ts
@@ -3,10 +3,10 @@ import logBase from 'loglevel';
 import { apply } from './logLevel';
 
 // @ts-expect-error - dont mutate global for Node.js
-export const log: logBase.Logger = global.log ?? apply(logBase.getLogger('default'));
+export const log: logBase.Logger = globalThis.log ?? apply(logBase.getLogger('default'));
 
 // remember this instance of prisma in development to avoid too many clients
 if (process.env.NODE_ENV === 'development') {
   // @ts-expect-error
-  global.log = log;
+  globalThis.log = log;
 }
