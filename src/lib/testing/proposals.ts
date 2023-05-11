60961176e47fd84e9bda980d33cd072e9fb7fc57
motechFR <mo@anyahq.co>
2023-05-12 00:34:10 +0300

    Fix generator (#16)

--- src/lib/testing/proposals.ts
+++ src/lib/testing/proposals.ts
@@ -2,10 +2,10 @@ import type { Page, Post, Prisma, ProposalCategory, ProposalStatus } from '@pris
 import { v4 } from 'uuid';
 
 import { prisma } from '../../db';
+import { randomThemeColor } from '../branding/colors';
 import { InvalidInputError } from '../errors';
 import type { ProposalCategoryPermissionAssignment } from '../permissions/proposals/interfaces';
 import type { ProposalReviewerInput, ProposalWithUsers } from '../proposals/interfaces';
-import { stringToColor } from '../utilities/strings';
 
 import { generatePage } from './pages';
 
@@ -22,7 +22,7 @@ export async function generateProposalCategory({
     data: {
       title,
       space: { connect: { id: spaceId } },
-      color: stringToColor(title),
+      color: randomThemeColor(),
       proposalCategoryPermissions:
         proposalCategoryPermissions && proposalCategoryPermissions.length > 0
           ? {
@@ -92,7 +92,7 @@ export async function generateProposal({
       proposal: {
         create: {
           category: !categoryId
-            ? { create: { color: stringToColor(title), title: `Category ${Math.random()}`, spaceId } }
+            ? { create: { color: randomThemeColor(), title: `Category ${Math.random()}`, spaceId } }
             : { connect: { id: categoryId } },
           id: proposalId,
           createdBy: userId,
