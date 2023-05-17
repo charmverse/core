1cbe2a9046815581d86ed1bda21f3d9631a78428
Matt Casey <mattwad@gmail.com>
2023-05-16 22:04:36 -0600

    update migrations

--- src/prisma/migrations/20230517040428_add_post_page_actions/migration.sql
+++ src/prisma/migrations/20230517040428_add_post_page_actions/migration.sql
@@ -1,50 +1,31 @@
-/*
-  Warnings:
+-- CreateEnum
+CREATE TYPE "PageActionType" AS ENUM ('view');
 
-  - You are about to drop the `UserPageAction` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `UserPostAction` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "UserPageAction" DROP CONSTRAINT "UserPageAction_createdBy_fkey";
-
--- DropForeignKey
-ALTER TABLE "UserPageAction" DROP CONSTRAINT "UserPageAction_pageId_fkey";
-
--- DropForeignKey
-ALTER TABLE "UserPageAction" DROP CONSTRAINT "UserPageAction_spaceId_fkey";
-
--- DropForeignKey
-ALTER TABLE "UserPostAction" DROP CONSTRAINT "UserPostAction_createdBy_fkey";
-
--- DropForeignKey
-ALTER TABLE "UserPostAction" DROP CONSTRAINT "UserPostAction_postId_fkey";
-
--- DropForeignKey
-ALTER TABLE "UserPostAction" DROP CONSTRAINT "UserPostAction_spaceId_fkey";
-
--- DropTable
-DROP TABLE "UserPageAction";
-
--- DropTable
-DROP TABLE "UserPostAction";
+-- CreateEnum
+CREATE TYPE "PostActionType" AS ENUM ('view');
 
 -- CreateTable
 CREATE TABLE "PageAction" (
+    "id" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdBy" UUID NOT NULL,
+    "createdBy" UUID,
     "pageId" UUID NOT NULL,
     "type" "PageActionType" NOT NULL,
-    "spaceId" UUID NOT NULL
+    "spaceId" UUID NOT NULL,
+
+    CONSTRAINT "PageAction_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PostAction" (
+    "id" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdBy" UUID NOT NULL,
+    "createdBy" UUID,
     "postId" UUID NOT NULL,
     "type" "PostActionType" NOT NULL,
-    "spaceId" UUID NOT NULL
+    "spaceId" UUID NOT NULL,
+
+    CONSTRAINT "PostAction_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -54,16 +35,10 @@ CREATE INDEX "PageAction_createdBy_type_idx" ON "PageAction"("createdBy", "type"
 CREATE INDEX "PageAction_spaceId_type_idx" ON "PageAction"("spaceId", "type");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "PageAction_createdBy_createdAt_key" ON "PageAction"("createdBy", "createdAt");
-
--- CreateIndex
 CREATE INDEX "PostAction_createdBy_type_idx" ON "PostAction"("createdBy", "type");
 
 -- CreateIndex
 CREATE INDEX "PostAction_spaceId_type_idx" ON "PostAction"("spaceId", "type");
-
--- CreateIndex
-CREATE UNIQUE INDEX "PostAction_createdBy_createdAt_key" ON "PostAction"("createdBy", "createdAt");
 
 -- AddForeignKey
 ALTER TABLE "PageAction" ADD CONSTRAINT "PageAction_pageId_fkey" FOREIGN KEY ("pageId") REFERENCES "Page"("id") ON DELETE CASCADE ON UPDATE CASCADE;
