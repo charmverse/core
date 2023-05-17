3dc5c28a00233f5d170b20e521669458fddf5546
Matt Casey <mattwad@gmail.com>
2023-05-17 09:32:26 -0600

    update schema

---
+++ src/prisma/migrations/20230517152546_add_user_space_actions/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "UserSpaceActionType" AS ENUM ('view_page');
+
+-- CreateTable
+CREATE TABLE "UserSpaceAction" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" UUID,
+    "pageId" UUID,
+    "postId" UUID,
+    "action" "UserSpaceActionType" NOT NULL,
+    "spaceId" UUID NOT NULL,
+
+    CONSTRAINT "UserSpaceAction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "UserSpaceAction_createdBy_action_idx" ON "UserSpaceAction"("createdBy", "action");
+
+-- CreateIndex
+CREATE INDEX "UserSpaceAction_spaceId_action_idx" ON "UserSpaceAction"("spaceId", "action");
+
+-- AddForeignKey
+ALTER TABLE "UserSpaceAction" ADD CONSTRAINT "UserSpaceAction_pageId_fkey" FOREIGN KEY ("pageId") REFERENCES "Page"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserSpaceAction" ADD CONSTRAINT "UserSpaceAction_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserSpaceAction" ADD CONSTRAINT "UserSpaceAction_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserSpaceAction" ADD CONSTRAINT "UserSpaceAction_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
