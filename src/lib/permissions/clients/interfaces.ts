a09b68717fc90106bed37e74b78b57493d2bf973
motechFR <mo@anyahq.co>
2023-05-22 21:32:21 +0200

    Extend API client

--- src/lib/permissions/clients/interfaces.ts
+++ src/lib/permissions/clients/interfaces.ts
@@ -1,17 +1,20 @@
 import type { BaseForumPermissionsClient, PremiumForumPermissionsClient } from '../forums/client/interfaces';
 import type { BasePagePermissionsClient, PremiumPagePermissionsClient } from '../pages/client/interfaces';
 import type { BaseProposalPermissionsClient, PremiumProposalPermissionsClient } from '../proposals/client/interfaces';
+import type { BaseSpacePermissionsClient, PremiumSpacePermissionsClient } from '../spaces/client/interfaces';
 
 export type PermissionsClient = {
   forum: BaseForumPermissionsClient;
   proposals: BaseProposalPermissionsClient;
   pages: BasePagePermissionsClient;
+  spaces: BaseSpacePermissionsClient;
 };
 
 export type PremiumPermissionsClient = {
   forum: PremiumForumPermissionsClient;
   proposals: PremiumProposalPermissionsClient;
   pages: PremiumPagePermissionsClient;
+  spaces: PremiumSpacePermissionsClient;
 };
 
 export type PermissionsApiClientConstructor = {
--- src/lib/permissions/clients/permissionsApiClient.class.ts
+++ src/lib/permissions/clients/permissionsApiClient.class.ts
@@ -4,6 +4,8 @@ import type { PremiumPagePermissionsClient } from '../pages/client/interfaces';
 import { PagePermissionsHttpClient } from '../pages/client/pagePermissionsHttpClient';
 import type { PremiumProposalPermissionsClient } from '../proposals/client/interfaces';
 import { ProposalPermissionsHttpClient } from '../proposals/client/proposalPermissionsHttpClient';
+import type { PremiumSpacePermissionsClient } from '../spaces/client/interfaces';
+import { SpacePermissionsHttpClient } from '../spaces/client/spacePermissionsHttpClient';
 
 import type { PermissionsApiClientConstructor, PremiumPermissionsClient } from './interfaces';
 
@@ -14,9 +16,12 @@ export class PermissionsApiClient implements PremiumPermissionsClient {
 
   pages: PremiumPagePermissionsClient;
 
+  spaces: PremiumSpacePermissionsClient;
+
   constructor(params: PermissionsApiClientConstructor) {
     this.forum = new ForumPermissionsHttpClient(params);
     this.proposals = new ProposalPermissionsHttpClient(params);
     this.pages = new PagePermissionsHttpClient(params);
+    this.spaces = new SpacePermissionsHttpClient(params);
   }
 }
