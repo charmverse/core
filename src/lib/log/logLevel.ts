fa8f3fbc09a4518a9d98351a0ee1de7963b0b819
Matt Casey <mattwad@gmail.com>
2023-05-24 23:00:57 -0600

    send browser logs to datadog

--- src/lib/log/logLevel.ts
+++ src/lib/log/logLevel.ts
@@ -1,3 +1,5 @@
+import type { Logger as DatadogLogger } from '@datadog/browser-logs';
+import { datadogLogs } from '@datadog/browser-logs';
 import { RateLimit } from 'async-sema';
 import type { Logger, LogLevelDesc } from 'loglevel';
 import _log from 'loglevel';
@@ -10,7 +12,12 @@ import { formatLog } from './logUtils';
 const ERRORS_WEBHOOK =
   'https://discord.com/api/webhooks/898365255703470182/HqS3KqH_7-_dj0KYR6EzNqWhkH0yX6kvV_P32sZ3gnvB8M4AyMoy7W9bbjIul3Hmyu98';
 const originalFactory = _log.methodFactory;
-const enableLogging = isProdEnv && isNodeEnv;
+const enableDiscordAlerts = (isStagingEnv || isProdEnv) && isNodeEnv;
+const enableDatadogLogs = isProdEnv && !isNodeEnv;
+
+const datadogLogMethods = ['log', 'debug', 'info', 'warn', 'error'] as const;
+type DatadogLogMethod = (typeof datadogLogMethods)[number];
+type DatadogLogFunction = DatadogLogger['log'];
 
 // requests per second = 35, timeUnit = 1sec
 const discordRateLimiter = RateLimit(30);
@@ -45,7 +52,7 @@ export function apply(log: Logger, logPrefix: string = '') {
         originalMethod.apply(null, args);
 
         // post errors to Discord
-        if (isProdEnv && methodName === 'error' && enableLogging) {
+        if (isProdEnv && methodName === 'error' && enableDiscordAlerts) {
           sendErrorToDiscord(ERRORS_WEBHOOK, message, opt).catch((error) => {
             logErrorPlain('Error posting to discord', { originalMessage: message, error });
           });
@@ -54,6 +61,17 @@ export function apply(log: Logger, logPrefix: string = '') {
     };
 
     log.setLevel(log.getLevel()); // Be sure to call setLevel method in order to apply plugin
+  } else if (enableDatadogLogs) {
+    log.methodFactory = (methodName, logLevel, loggerName) => {
+      const originalMethod = originalFactory(methodName, logLevel, loggerName);
+      return (message, ...args) => {
+        originalMethod.apply(null, [message, ...args]);
+        const firstArg = args[0];
+        const error = firstArg instanceof Error ? firstArg : (firstArg as any).error || firstArg;
+        const datadogLog = datadogLogs.logger[methodName as DatadogLogMethod] as DatadogLogFunction;
+        datadogLog?.(message, args[0], error);
+      };
+    };
   }
 
   return log;
