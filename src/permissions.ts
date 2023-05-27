960f4e461753653727a76f7c9f8607d66906ab33
motechFR <mo@anyahq.co>
2023-05-27 14:17:44 +0200

    Fix the export

--- src/permissions.ts
+++ src/permissions.ts
@@ -1,4 +1,1 @@
-export * as PermissionTypes from './lib/permissions/interfaces';
-
-export { AvailablePagePermissions } from './lib/permissions/pages/availablePagePermissions.class';
-export { PagePermissionsHttpClient } from './lib/permissions/pages/client/pagePermissionsHttpClient';
+export * from './lib/permissions/index';
