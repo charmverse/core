dd0becd1e53dcb8a275d6992273c8275c9c0dfba
Valentin L <ludu.valentin@gmail.com>
2023-06-20 18:10:15 +0300

    Keep just quantity

--- src/lib/testing/__tests__/user.spec.ts
+++ src/lib/testing/__tests__/user.spec.ts
@@ -9,7 +9,7 @@ describe('generateUserAndSpace', () => {
 
     expect(space.publicBountyBoard).toBe(false);
     expect(space.publicProposals).toBe(false);
-    expect(space.paidTier).toBe('community');
+    expect(space.paidTier).toBe('pro');
 
     const spaceRole = (await prisma.spaceRole.findFirst({
       where: {
