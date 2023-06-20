7175fcab7bf73731847133140d28cc82d3e85f6c
Valentin L <ludu.valentin@gmail.com>
2023-06-20 14:04:41 +0300

    Add the community package

---
+++ src/prisma/migrations/20230620110156_update_subs/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - The values [pro] on the enum `SubscriptionTier` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "SubscriptionTier_new" AS ENUM ('free', 'community', 'enterprise');
+ALTER TABLE "Space" ALTER COLUMN "paidTier" DROP DEFAULT;
+ALTER TABLE "Space" ALTER COLUMN "paidTier" TYPE "SubscriptionTier_new" USING ("paidTier"::text::"SubscriptionTier_new");
+ALTER TYPE "SubscriptionTier" RENAME TO "SubscriptionTier_old";
+ALTER TYPE "SubscriptionTier_new" RENAME TO "SubscriptionTier";
+DROP TYPE "SubscriptionTier_old";
+ALTER TABLE "Space" ALTER COLUMN "paidTier" SET DEFAULT 'community';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "Space" ALTER COLUMN "paidTier" SET DEFAULT 'community';
+
+-- AlterTable
+ALTER TABLE "StripeSubscription" ADD COLUMN     "quantity" INTEGER NOT NULL DEFAULT 1;
