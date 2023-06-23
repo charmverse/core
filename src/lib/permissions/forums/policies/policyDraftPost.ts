6ddad9ca367f4dae188a326659254f2c92a33620
motechFR <mo@anyahq.co>
2023-06-23 19:22:47 +0300

    Refactor the draft post policy

--- src/lib/permissions/forums/policies/policyDraftPost.ts
+++ src/lib/permissions/forums/policies/policyDraftPost.ts
@@ -1,18 +1,18 @@
+import { AvailablePostPermissions } from '../availablePostPermissions.class';
 import type { PostPermissionFlags } from '../interfaces';
 
 import type { PostPolicyInput } from './interfaces';
 
 export async function policyDraftPost({ resource, flags, userId }: PostPolicyInput): Promise<PostPermissionFlags> {
+  if (!resource.isDraft) {
+    return flags;
+  }
+
   const newPermissions = {
-    edit_post: userId && resource.isDraft ? resource.createdBy === userId : flags.edit_post,
-    pin_post: userId && resource.isDraft ? false : flags.pin_post,
-    lock_post: userId && resource.isDraft ? false : flags.lock_post,
-    delete_comments: resource.isDraft ? false : flags.delete_comments,
-    add_comment: resource.isDraft ? false : flags.add_comment,
-    upvote: resource.isDraft ? false : flags.upvote,
-    downvote: resource.isDraft ? false : flags.downvote,
-    delete_post: userId && resource.isDraft ? resource.createdBy === userId : flags.delete_post,
-    view_post: userId && resource.isDraft ? resource.createdBy === userId : flags.view_post
+    ...new AvailablePostPermissions().empty,
+    edit_post: resource.createdBy === userId,
+    view_post: resource.createdBy === userId,
+    delete_post: resource.createdBy === userId
   };
 
   return newPermissions;
