2d077f52349fb6f79a35da5b88a89f3bbadf6e30
Valentin L <ludu.valentin@gmail.com>
2023-08-15 14:44:21 +0300

    Rename to allowDiscovery

--- src/prisma/migrations/20230815091604_add_page_permission_visibility/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "PagePermission" ADD COLUMN     "isVisible" BOOLEAN DEFAULT true;
---
+++ src/prisma/migrations/20230815114353_add_allow_discovery_page_permission/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "PagePermission" ADD COLUMN     "allowDiscovery" BOOLEAN DEFAULT true;
