2dbc0720ac64bc462dec487feabf6c1fba3c0a6e
Safwan Shaheer <devorein00@gmail.com>
2023-08-23 23:18:41 +0600

    Final schema changes

--- src/prisma/migrations/20230821120539_auto_lens_publish/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "User_xpsEngineId_key";
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "autoLensPublish" BOOLEAN DEFAULT false;
--- src/prisma/migrations/20230823171619_proposal_lens_publish/migration.sql
+++ src/prisma/migrations/20230823171619_proposal_lens_publish/migration.sql
@@ -1,3 +1,6 @@
 -- AlterTable
 ALTER TABLE "Proposal" ADD COLUMN     "lensPostLink" TEXT,
 ADD COLUMN     "publishToLens" BOOLEAN;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "autoLensPublish" BOOLEAN DEFAULT false;
