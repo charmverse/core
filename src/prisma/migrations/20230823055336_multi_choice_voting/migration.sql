56654dff94dc33eda7c8727cabf7a9e10c58977a
Marek <m.sadura@outlook.com>
2023-08-23 07:54:17 +0200

    add new vote type

--- src/prisma/migrations/20230823055336_multi_choice_voting/migration.sql
+++ src/prisma/migrations/20230823055336_multi_choice_voting/migration.sql
@@ -1,3 +1,6 @@
+-- AlterEnum
+ALTER TYPE "VoteType" ADD VALUE 'MultiChoice';
+
 -- AlterTable
 ALTER TABLE "UserVote" ADD COLUMN     "choices" TEXT[] DEFAULT ARRAY[]::TEXT[],
 ALTER COLUMN "choice" DROP NOT NULL;
