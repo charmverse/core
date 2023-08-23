30dd0a85b7afe17e9fe412834fdf04475c0a61cc
Safwan Shaheer <devorein00@gmail.com>
2023-08-24 01:56:57 +0600

    Renamed autoLensPublish to publishToLensDefault

--- src/prisma/migrations/20230823195435_lens_publish/migration.sql
+++ src/prisma/migrations/20230823195435_lens_publish/migration.sql
@@ -3,4 +3,4 @@ ALTER TABLE "Proposal" ADD COLUMN     "lensPostLink" TEXT,
 ADD COLUMN     "publishToLens" BOOLEAN;
 
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "autoLensPublish" BOOLEAN DEFAULT false;
+ALTER TABLE "User" ADD COLUMN     "publishToLensDefault" BOOLEAN DEFAULT false;
