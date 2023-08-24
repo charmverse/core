def6e599a49f5b893b7e3b9e26033215b5704f51
Marek <m.sadura@outlook.com>
2023-08-24 09:59:14 +0200

    Feat/multi choice voting (#102)
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20230823055336_multi_choice_voting/migration.sql
@@ -0,0 +1,9 @@
+-- AlterEnum
+ALTER TYPE "VoteType" ADD VALUE 'MultiChoice';
+
+-- AlterTable
+ALTER TABLE "UserVote" ADD COLUMN     "choices" TEXT[] DEFAULT ARRAY[]::TEXT[],
+ALTER COLUMN "choice" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Vote" ADD COLUMN     "maxChoices" INTEGER DEFAULT 1;
