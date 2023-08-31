9d76759a4ff607121b71698f3de8f9cf6504e6e2
Safwan Shaheer <devorein00@gmail.com>
2023-08-31 18:20:18 +0600

    Dropped choice from uservote model

---
+++ src/prisma/migrations/20230831121612_remove_uservote_choice/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `choice` on the `UserVote` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserVote" DROP COLUMN "choice";
