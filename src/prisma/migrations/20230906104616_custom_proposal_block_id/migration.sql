1b4d137ae87dfa73f45fefd78dabe14536959c94
Marek <m.sadura@outlook.com>
2023-09-06 12:52:18 +0200

    Fix migration

--- src/prisma/migrations/20230906104616_custom_proposal_block_id/migration.sql
+++ src/prisma/migrations/20230906104616_custom_proposal_block_id/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - The primary key for the `ProposalBlock` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - A unique constraint covering the columns `[id,spaceId]` on the table `ProposalBlock` will be added. If there are existing duplicate values, this will fail.
   - Changed the type of `type` on the `ProposalBlock` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
 */
@@ -10,7 +9,8 @@
 ALTER TABLE "ProposalBlock" DROP CONSTRAINT "ProposalBlock_pkey",
 ALTER COLUMN "id" SET DATA TYPE TEXT,
 DROP COLUMN "type",
-ADD COLUMN     "type" TEXT NOT NULL;
+ADD COLUMN     "type" TEXT NOT NULL,
+ADD CONSTRAINT "ProposalBlock_pkey" PRIMARY KEY ("id", "spaceId");
 
--- CreateIndex
-CREATE UNIQUE INDEX "ProposalBlock_id_spaceId_key" ON "ProposalBlock"("id", "spaceId");
+-- DropEnum
+DROP TYPE "ProposalBlockType";
