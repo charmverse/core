c43576bbfb25fecbf7045b1f88bba87c2cf07e08
motechFR <mo@anyahq.co>
2023-10-14 04:25:52 +0300

    Update fields

--- src/prisma/migrations/20231013044920_drop_unique_application_constraint_add_application_reward/migration.sql
+++ src/prisma/migrations/20231013044920_drop_unique_application_constraint_add_application_reward/migration.sql
@@ -2,4 +2,7 @@
 DROP INDEX "Application_bountyId_createdBy_key";
 
 -- AlterTable
+ALTER TABLE "Application" ADD COLUMN     "rewardInfo" TEXT;
+
+-- AlterTable
 ALTER TABLE "Bounty" ADD COLUMN     "allowMultipleApplications" BOOLEAN DEFAULT false;
