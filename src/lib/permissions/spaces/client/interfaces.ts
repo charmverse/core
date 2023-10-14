e939ea62298c3722f1948b90a9b6f70f413bf617
motechFR <mo@anyahq.co>
2023-10-15 00:08:29 +0300

    Deprecate list available bounties

--- src/lib/permissions/spaces/client/interfaces.ts
+++ src/lib/permissions/spaces/client/interfaces.ts
@@ -1,13 +1,10 @@
 import type { Space } from '@prisma/client';
 
-import type { BountyWithDetails } from '../../../bounties/interfaces';
-import type { PermissionCompute, SpaceResourcesRequest } from '../../core/interfaces';
+import type { PermissionCompute } from '../../core/interfaces';
 import type { PublicBountyToggle, SpaceDefaultPublicPageToggle, SpacePermissionFlags } from '../interfaces';
 
 export type BaseSpacePermissionsClient = {
   computeSpacePermissions: (request: PermissionCompute) => Promise<SpacePermissionFlags>;
-
-  listAvailableBounties: (request: SpaceResourcesRequest) => Promise<BountyWithDetails[]>;
 };
 export type PremiumSpacePermissionsClient = BaseSpacePermissionsClient & {
   toggleSpaceDefaultPublicPage: (request: SpaceDefaultPublicPageToggle) => Promise<Space>;
