5f47ab26bfd806f88ca139ae08e11e174554922e
Matt Casey <mattwad@gmail.com>
2023-10-17 12:30:18 -0600

    bring back bounty interface

--- src/lib/permissions/spaces/client/interfaces.ts
+++ src/lib/permissions/spaces/client/interfaces.ts
@@ -1,10 +1,12 @@
 import type { Space } from '@prisma/client';
 
+import type { BountyWithDetails } from '../../../bounties/interfaces';
 import type { PermissionCompute } from '../../core/interfaces';
 import type { PublicBountyToggle, SpaceDefaultPublicPageToggle, SpacePermissionFlags } from '../interfaces';
 
 export type BaseSpacePermissionsClient = {
   computeSpacePermissions: (request: PermissionCompute) => Promise<SpacePermissionFlags>;
+  listAvailableBounties: (request: SpaceResourcesRequest) => Promise<BountyWithDetails[]>; // TODO: remove this after rewards have replaced bounties in webapp
 };
 export type PremiumSpacePermissionsClient = BaseSpacePermissionsClient & {
   toggleSpaceDefaultPublicPage: (request: SpaceDefaultPublicPageToggle) => Promise<Space>;
