1c737dce9e815413eefc75d6f4cf01c2e72d8d6c
Matt Casey <mattwad@gmail.com>
2023-10-17 12:30:50 -0600

    fix ts error

--- src/lib/permissions/spaces/client/interfaces.ts
+++ src/lib/permissions/spaces/client/interfaces.ts
@@ -1,7 +1,7 @@
 import type { Space } from '@prisma/client';
 
 import type { BountyWithDetails } from '../../../bounties/interfaces';
-import type { PermissionCompute } from '../../core/interfaces';
+import type { PermissionCompute, SpaceResourcesRequest } from '../../core/interfaces';
 import type { PublicBountyToggle, SpaceDefaultPublicPageToggle, SpacePermissionFlags } from '../interfaces';
 
 export type BaseSpacePermissionsClient = {
