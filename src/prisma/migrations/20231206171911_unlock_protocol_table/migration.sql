64cc01ec86294a76a5b044151808803e6d0b650b
Valentin L <ludu.valentin@gmail.com>
2023-12-06 19:19:28 +0200

    New table for gatetoroles

--- src/prisma/migrations/20231206171911_unlock_protocol_table/migration.sql
+++ src/prisma/migrations/20231206171911_unlock_protocol_table/migration.sql
@@ -2,9 +2,6 @@
 ALTER TABLE "SpaceRole" ADD COLUMN     "unlockProtocolGateId" UUID;
 
 -- AlterTable
-ALTER TABLE "TokenGateToRole" ADD COLUMN     "unlockProtocolGateId" UUID;
-
--- AlterTable
 ALTER TABLE "UserTokenGate" ADD COLUMN     "unlockProtocolGateId" UUID;
 
 -- CreateTable
@@ -17,12 +14,32 @@ CREATE TABLE "UnlockProtocolGate" (
     "chainId" INTEGER NOT NULL,
     "userRole" TEXT,
     "accessTypes" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "unlockProtocolGateToRoleId" TEXT NOT NULL,
 
     CONSTRAINT "UnlockProtocolGate_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "UnlockProtocolGateToRole" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "roleId" UUID NOT NULL,
+    "unlockProtocolGateId" UUID NOT NULL,
+
+    CONSTRAINT "UnlockProtocolGateToRole_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE INDEX "UnlockProtocolGate_spaceId_idx" ON "UnlockProtocolGate"("spaceId");
+
+-- CreateIndex
+CREATE INDEX "UnlockProtocolGateToRole_unlockProtocolGateId_idx" ON "UnlockProtocolGateToRole"("unlockProtocolGateId");
+
+-- CreateIndex
+CREATE INDEX "UnlockProtocolGateToRole_roleId_idx" ON "UnlockProtocolGateToRole"("roleId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UnlockProtocolGateToRole_unlockProtocolGateId_roleId_key" ON "UnlockProtocolGateToRole"("unlockProtocolGateId", "roleId");
 
 -- AddForeignKey
 ALTER TABLE "UserTokenGate" ADD CONSTRAINT "UserTokenGate_unlockProtocolGateId_fkey" FOREIGN KEY ("unlockProtocolGateId") REFERENCES "UnlockProtocolGate"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -34,4 +51,7 @@ ALTER TABLE "SpaceRole" ADD CONSTRAINT "SpaceRole_unlockProtocolGateId_fkey" FOR
 ALTER TABLE "UnlockProtocolGate" ADD CONSTRAINT "UnlockProtocolGate_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "TokenGateToRole" ADD CONSTRAINT "TokenGateToRole_unlockProtocolGateId_fkey" FOREIGN KEY ("unlockProtocolGateId") REFERENCES "UnlockProtocolGate"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "UnlockProtocolGateToRole" ADD CONSTRAINT "UnlockProtocolGateToRole_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UnlockProtocolGateToRole" ADD CONSTRAINT "UnlockProtocolGateToRole_unlockProtocolGateId_fkey" FOREIGN KEY ("unlockProtocolGateId") REFERENCES "UnlockProtocolGate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
