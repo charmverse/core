fff29863be1f12a6f46a215bcaf910841c6b8622
Valentin L <ludu.valentin@gmail.com>
2023-12-08 11:26:39 +0200

    New types

--- src/prisma/migrations/20231208092617_add_tokengate_type/migration.sql
+++ src/prisma/migrations/20231208092617_add_tokengate_type/migration.sql
@@ -4,5 +4,8 @@
   - Added the required column `type` to the `TokenGate` table without a default value. This is not possible if the table is not empty.
 
 */
+-- CreateEnum
+CREATE TYPE "TokenGateType" AS ENUM ('lit', 'unlock');
+
 -- AlterTable
-ALTER TABLE "TokenGate" ADD COLUMN     "type" TEXT NOT NULL;
+ALTER TABLE "TokenGate" ADD COLUMN     "type" "TokenGateType" NOT NULL;
