405006203383237ce2d654a3a0ae1a001117f4f0
Safwan Shaheer <devorein00@gmail.com>
2023-12-13 10:08:50 +0600

    Added more identity types

--- src/prisma/migrations/20231213040736_additional_identity_type/migration.sql
+++ src/prisma/migrations/20231213040736_additional_identity_type/migration.sql
@@ -1,5 +1,13 @@
--- CreateEnum
-CREATE TYPE "PrimaryMemberIdentity" AS ENUM ('google', 'wallet', 'discord', 'telegram');
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "IdentityType" ADD VALUE 'Lens';
+ALTER TYPE "IdentityType" ADD VALUE 'Farcaster';
 
 -- AlterEnum
 -- This migration adds more than one value to an enum.
@@ -14,4 +22,4 @@ ALTER TYPE "MemberPropertyType" ADD VALUE 'telegram';
 ALTER TYPE "MemberPropertyType" ADD VALUE 'wallet';
 
 -- AlterTable
-ALTER TABLE "Space" ADD COLUMN     "primaryMemberIdentity" "PrimaryMemberIdentity";
+ALTER TABLE "Space" ADD COLUMN     "primaryMemberIdentity" "IdentityType";
