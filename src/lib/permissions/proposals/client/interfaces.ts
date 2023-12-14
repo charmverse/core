b5018734c29664033cebe6ea907e0f7fb54ec8d9
motechFR <mo@anyahq.co>
2023-12-14 22:14:24 +0200

    Fix interfaces

--- src/lib/permissions/proposals/client/interfaces.ts
+++ src/lib/permissions/proposals/client/interfaces.ts
@@ -12,17 +12,18 @@ import type {
 import type { ProposalFlowPermissionFlags } from '../proposalFlowFlags';
 
 export type BaseProposalPermissionsClient = {
-  computeProposalPermissions: (request: PermissionCompute) => Promise<ProposalPermissionFlags>;
+  computeProposalPermissions: (
+    request: PermissionCompute & ProposalPermissionsSwitch
+  ) => Promise<ProposalPermissionFlags>;
   computeProposalCategoryPermissions: (request: PermissionCompute) => Promise<ProposalCategoryPermissionFlags>;
   computeProposalFlowPermissions: (request: PermissionCompute) => Promise<ProposalFlowPermissionFlags>;
   getAccessibleProposalCategories: (request: SpaceResourcesRequest) => Promise<ProposalCategoryWithPermissions[]>;
-  getAccessibleProposalIds: (request: ListProposalsRequest & ProposalPermissionsSwitch) => Promise<string[]>;
   getProposalReviewerPool: (request: Resource) => Promise<ProposalReviewerPool>;
 };
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type PremiumProposalPermissionsClient = BaseProposalPermissionsClient & {
   // This will be the new method used for proposals with evaluation step
-  computeProposalEvaluationPermissions: (request: PermissionCompute) => Promise<ProposalPermissionFlags>;
+  getAccessibleProposalIds: (request: ListProposalsRequest & ProposalPermissionsSwitch) => Promise<string[]>;
   computeBaseProposalPermissions: (request: PermissionCompute) => Promise<ProposalPermissionFlags>;
   assignDefaultProposalCategoryPermissions: (proposalCategory: Resource) => Promise<void>;
   upsertProposalCategoryPermission: (
