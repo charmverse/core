ceef1e7bc1ae3cde82019d077d00553f15da91d8
mattcasey <mattwad@gmail.com>
2024-01-22 22:53:16 -0700

    require evaluation id on proposal relations

---
+++ src/prisma/migrations/20240123055259_require_evaluation_id/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - Made the column `evaluationId` on table `ProposalReviewer` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `evaluationId` on table `ProposalRubricCriteria` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `evaluationId` on table `ProposalRubricCriteriaAnswer` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "ProposalRubricCriteria" DROP CONSTRAINT "ProposalRubricCriteria_evaluationId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ProposalRubricCriteriaAnswer" DROP CONSTRAINT "ProposalRubricCriteriaAnswer_evaluationId_fkey";
+
+-- AlterTable
+ALTER TABLE "ProposalReviewer" ALTER COLUMN "evaluationId" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "ProposalRubricCriteria" ALTER COLUMN "evaluationId" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "ProposalRubricCriteriaAnswer" ALTER COLUMN "evaluationId" SET NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "ProposalRubricCriteria" ADD CONSTRAINT "ProposalRubricCriteria_evaluationId_fkey" FOREIGN KEY ("evaluationId") REFERENCES "ProposalEvaluation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProposalRubricCriteriaAnswer" ADD CONSTRAINT "ProposalRubricCriteriaAnswer_evaluationId_fkey" FOREIGN KEY ("evaluationId") REFERENCES "ProposalEvaluation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
