0d30afd16e446fec2da0c0b376365b3fa31f2649
Safwan Shaheer <devorein00@gmail.com>
2024-02-01 21:05:30 +0600

    Added unique index and userId relation

--- src/prisma/migrations/20240201150512_favorite_credential/migration.sql
+++ src/prisma/migrations/20240201150512_favorite_credential/migration.sql
@@ -2,18 +2,20 @@
 CREATE TABLE "FavoriteCredential" (
     "id" UUID NOT NULL,
     "index" INTEGER NOT NULL DEFAULT -1,
-    "attestationId" TEXT,
     "chainId" INTEGER,
+    "userId" UUID NOT NULL,
+    "attestationId" TEXT,
     "issuedCredentialId" UUID,
+    "gitcoinWalletAddress" TEXT,
 
     CONSTRAINT "FavoriteCredential_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "FavoriteCredential_issuedCredentialId_key" ON "FavoriteCredential"("issuedCredentialId");
+CREATE UNIQUE INDEX "FavoriteCredential_issuedCredentialId_attestationId_gitcoin_key" ON "FavoriteCredential"("issuedCredentialId", "attestationId", "gitcoinWalletAddress");
 
--- CreateIndex
-CREATE UNIQUE INDEX "FavoriteCredential_attestationId_key" ON "FavoriteCredential"("attestationId");
+-- AddForeignKey
+ALTER TABLE "FavoriteCredential" ADD CONSTRAINT "FavoriteCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "FavoriteCredential" ADD CONSTRAINT "FavoriteCredential_issuedCredentialId_fkey" FOREIGN KEY ("issuedCredentialId") REFERENCES "IssuedCredential"("id") ON DELETE SET NULL ON UPDATE CASCADE;
