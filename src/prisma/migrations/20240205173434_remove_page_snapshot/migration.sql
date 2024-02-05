fc3949084f35259891e327d97b02cb6ff02b5d06
mattcasey <mattwad@gmail.com>
2024-02-05 10:35:19 -0700

    Revert "remove snapshotProposalId from pages"
    This reverts commit 9124209ad3d8563f82d0e6a683eec4d09d63968e.

--- src/prisma/migrations/20240205173434_remove_page_snapshot/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `snapshotProposalId` on the `Page` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Page" DROP COLUMN "snapshotProposalId";
