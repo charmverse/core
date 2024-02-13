aa7f48998c199c0c590d77dd6a8a12a0950df130
mattcasey <mattwad@gmail.com>
2024-02-13 11:10:56 -0700

    .

--- src/prisma/migrations/20240213181051_remove_proposal_operations/migration.sql
+++ src/prisma/migrations/20240213181051_remove_proposal_operations/migration.sql
@@ -1,12 +1,12 @@
 /*
   Warnings:
 
-  - The values [delete,create_vote,vote,review,make_public,archive,unarchive] on the enum `ProposalOperation` will be removed. If these variants are still used in the database, this will fail.
+  - The values [vote,review,make_public] on the enum `ProposalOperation` will be removed. If these variants are still used in the database, this will fail.
 
 */
 -- AlterEnum
 BEGIN;
-CREATE TYPE "ProposalOperation_new" AS ENUM ('edit', 'view', 'comment', 'evaluate', 'move');
+CREATE TYPE "ProposalOperation_new" AS ENUM ('edit', 'view', 'view_private_fields', 'delete', 'create_vote', 'comment', 'evaluate', 'archive', 'unarchive', 'move');
 ALTER TABLE "ProposalEvaluationPermission" ALTER COLUMN "operation" TYPE "ProposalOperation_new" USING ("operation"::text::"ProposalOperation_new");
 ALTER TYPE "ProposalOperation" RENAME TO "ProposalOperation_old";
 ALTER TYPE "ProposalOperation_new" RENAME TO "ProposalOperation";
