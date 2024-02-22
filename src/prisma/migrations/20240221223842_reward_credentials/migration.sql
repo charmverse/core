55589201801adfda56f6e3092952eb796c15356e
motechFR <mo@anyahq.co>
2024-02-22 11:39:46 +0200

    Cleanup migrations

--- src/prisma/migrations/20240221223842_reward_credentials/migration.sql
+++ src/prisma/migrations/20240221223842_reward_credentials/migration.sql
@@ -3,3 +3,6 @@ ALTER TYPE "AttestationType" ADD VALUE 'reward';
 
 -- AlterEnum
 ALTER TYPE "CredentialEventType" ADD VALUE 'reward_submission_approved';
+
+-- AlterTable
+ALTER TABLE "Bounty" ADD COLUMN     "selectedCredentialTemplates" TEXT[];
