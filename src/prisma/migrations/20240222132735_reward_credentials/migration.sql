f19f721c334103568ae3776095579c505828b84e
motechFR <mo@anyahq.co>
2024-02-22 15:27:50 +0200

    Fix model

--- src/prisma/migrations/20240222132735_reward_credentials/migration.sql
+++ src/prisma/migrations/20240222132735_reward_credentials/migration.sql
@@ -8,8 +8,8 @@ ALTER TYPE "CredentialEventType" ADD VALUE 'reward_submission_approved';
 ALTER TABLE "Bounty" ADD COLUMN     "selectedCredentialTemplates" TEXT[];
 
 -- AlterTable
-ALTER TABLE "IssuedCredential" ADD COLUMN     "rewardId" UUID,
+ALTER TABLE "IssuedCredential" ADD COLUMN     "applicationId" UUID,
 ALTER COLUMN "proposalId" DROP NOT NULL;
 
 -- AddForeignKey
-ALTER TABLE "IssuedCredential" ADD CONSTRAINT "IssuedCredential_rewardId_fkey" FOREIGN KEY ("rewardId") REFERENCES "Bounty"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "IssuedCredential" ADD CONSTRAINT "IssuedCredential_applicationId_fkey" FOREIGN KEY ("applicationId") REFERENCES "Application"("id") ON DELETE CASCADE ON UPDATE CASCADE;
