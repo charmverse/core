2f23194b7881f16be2c2a5df4e92bd48cd07b2c1
motechFR <mo@anyahq.co>
2024-02-27 09:53:20 -0700

    Cleanup model names

--- src/prisma/migrations/20240227165300_reward_credentials/migration.sql
+++ src/prisma/migrations/20240227165300_reward_credentials/migration.sql
@@ -8,8 +8,8 @@ ALTER TYPE "CredentialEventType" ADD VALUE 'reward_submission_approved';
 ALTER TABLE "Bounty" ADD COLUMN     "selectedCredentialTemplates" TEXT[];
 
 -- AlterTable
-ALTER TABLE "IssuedCredential" ADD COLUMN     "applicationId" UUID,
+ALTER TABLE "IssuedCredential" ADD COLUMN     "rewardApplicationId" UUID,
 ALTER COLUMN "proposalId" DROP NOT NULL;
 
 -- AddForeignKey
-ALTER TABLE "IssuedCredential" ADD CONSTRAINT "IssuedCredential_applicationId_fkey" FOREIGN KEY ("applicationId") REFERENCES "Application"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "IssuedCredential" ADD CONSTRAINT "IssuedCredential_rewardApplicationId_fkey" FOREIGN KEY ("rewardApplicationId") REFERENCES "Application"("id") ON DELETE CASCADE ON UPDATE CASCADE;
