5f11dcc2ae65854635134d9e1c0d1ce6e621ae37
Safwan Shaheer <devorein00@gmail.com>
2024-03-06 21:07:21 +0600

    Move blockNumber to vote model

--- src/prisma/migrations/20240306150652_proposal_token_voting/migration.sql
+++ src/prisma/migrations/20240306150652_proposal_token_voting/migration.sql
@@ -1,9 +1,7 @@
 -- AlterTable
-ALTER TABLE "Proposal" ADD COLUMN     "blockNumber" INTEGER;
-
--- AlterTable
 ALTER TABLE "UserVote" ADD COLUMN     "tokenAmount" DOUBLE PRECISION;
 
 -- AlterTable
-ALTER TABLE "Vote" ADD COLUMN     "chainId" INTEGER,
+ALTER TABLE "Vote" ADD COLUMN     "blockNumber" INTEGER,
+ADD COLUMN     "chainId" INTEGER,
 ADD COLUMN     "tokenAddress" TEXT;
