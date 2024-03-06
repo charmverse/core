109c1ef812211a14f675bae888e295471689fedc
Safwan Shaheer <devorein00@gmail.com>
2024-03-06 20:58:45 +0600

    Added tokenAddress and chainId for vote schema

--- src/prisma/migrations/20240306145824_proposal_token_voting/migration.sql
+++ src/prisma/migrations/20240306145824_proposal_token_voting/migration.sql
@@ -3,3 +3,7 @@ ALTER TABLE "Proposal" ADD COLUMN     "blockNumber" INTEGER;
 
 -- AlterTable
 ALTER TABLE "UserVote" ADD COLUMN     "tokenAmount" DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "Vote" ADD COLUMN     "chainId" INTEGER,
+ADD COLUMN     "tokenAddress" TEXT;
