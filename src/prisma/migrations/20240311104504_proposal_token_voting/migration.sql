9c1e55c374fab9c87fcf053ecd48485fa22669e3
Safwan Shaheer <devorein00@gmail.com>
2024-03-11 16:45:20 +0600

    Added strategy enum for vote

--- src/prisma/migrations/20240311104504_proposal_token_voting/migration.sql
+++ src/prisma/migrations/20240311104504_proposal_token_voting/migration.sql
@@ -1,7 +1,11 @@
+-- CreateEnum
+CREATE TYPE "VoteStrategy" AS ENUM ('regular', 'token', 'snapshot');
+
 -- AlterTable
 ALTER TABLE "UserVote" ADD COLUMN     "tokenAmount" DOUBLE PRECISION;
 
 -- AlterTable
 ALTER TABLE "Vote" ADD COLUMN     "blockNumber" INTEGER,
 ADD COLUMN     "chainId" INTEGER,
+ADD COLUMN     "strategy" "VoteStrategy" NOT NULL DEFAULT 'regular',
 ADD COLUMN     "tokenAddress" TEXT;
