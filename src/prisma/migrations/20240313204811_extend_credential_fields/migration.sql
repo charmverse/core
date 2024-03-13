ced80ea989be5002d9fe04c83c3f117341385328
motechFR <mo@anyahq.co>
2024-03-13 22:48:25 +0200

    Adjust naming conventions

--- src/prisma/migrations/20240313204811_extend_credential_fields/migration.sql
+++ src/prisma/migrations/20240313204811_extend_credential_fields/migration.sql
@@ -2,13 +2,13 @@
   Warnings:
 
   - You are about to drop the column `issueCredentialsOnChainId` on the `Space` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[onChainAttestationId]` on the table `IssuedCredential` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[onchainAttestationId]` on the table `IssuedCredential` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- AlterTable
 ALTER TABLE "IssuedCredential" ADD COLUMN     "ceramicRecord" JSONB,
-ADD COLUMN     "easChainId" INTEGER,
-ADD COLUMN     "onChainAttestationId" TEXT,
+ADD COLUMN     "onchainAttestationId" TEXT,
+ADD COLUMN     "onchainChainId" INTEGER,
 ADD COLUMN     "schemaId" TEXT,
 ALTER COLUMN "ceramicId" DROP NOT NULL;
 
@@ -20,7 +20,7 @@ ADD COLUMN     "credentialsChainId" INTEGER;
 CREATE INDEX "CharmWallet_totalBalance_idx" ON "CharmWallet"("totalBalance" DESC);
 
 -- CreateIndex
-CREATE UNIQUE INDEX "IssuedCredential_onChainAttestationId_key" ON "IssuedCredential"("onChainAttestationId");
+CREATE UNIQUE INDEX "IssuedCredential_onchainAttestationId_key" ON "IssuedCredential"("onchainAttestationId");
 
 -- CreateIndex
 CREATE INDEX "User_createdAt_idx" ON "User"("createdAt" ASC);
