420e726fa1be6226f3da27b1bbfdbca6ce810bb8
Safwan Shaheer <devorein00@gmail.com>
2024-03-14 12:13:05 +0600

    Use string for vote tokenAmount

--- src/prisma/migrations/20240314061253_proposal_token_voting/migration.sql
+++ src/prisma/migrations/20240314061253_proposal_token_voting/migration.sql
@@ -2,10 +2,16 @@
 CREATE TYPE "VoteStrategy" AS ENUM ('regular', 'token', 'snapshot');
 
 -- AlterTable
-ALTER TABLE "UserVote" ADD COLUMN     "tokenAmount" DOUBLE PRECISION;
+ALTER TABLE "UserVote" ADD COLUMN     "tokenAmount" TEXT;
 
 -- AlterTable
 ALTER TABLE "Vote" ADD COLUMN     "blockNumber" TEXT,
 ADD COLUMN     "chainId" INTEGER,
 ADD COLUMN     "strategy" "VoteStrategy" NOT NULL DEFAULT 'regular',
 ADD COLUMN     "tokenAddress" TEXT;
+
+-- CreateIndex
+CREATE INDEX "CharmWallet_totalBalance_idx" ON "CharmWallet"("totalBalance" DESC);
+
+-- CreateIndex
+CREATE INDEX "User_createdAt_idx" ON "User"("createdAt" ASC);
