79bd346740e9b657ca3a46e4bb72744a48b63ffb
Valentin L <ludu.valentin@gmail.com>
2024-03-20 18:35:43 +0200

    Add external projects

---
+++ src/prisma/migrations/20240320163503_extend_attestation/migration.sql
@@ -0,0 +1,17 @@
+-- AlterEnum
+ALTER TYPE "AttestationType" ADD VALUE 'external';
+
+-- CreateTable
+CREATE TABLE "ExternalProjects" (
+    "id" UUID NOT NULL,
+    "metadata" JSONB NOT NULL,
+    "recipient" TEXT NOT NULL,
+    "schemaId" TEXT,
+    "ceramicId" TEXT,
+    "ceramicRecord" JSONB,
+
+    CONSTRAINT "ExternalProjects_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ExternalProjects_ceramicId_key" ON "ExternalProjects"("ceramicId");
