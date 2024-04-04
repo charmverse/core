e97b6e7cf2ed80bd6f53537107208ed03acdda0d
motechFR <mo@anyahq.co>
2024-04-04 03:28:53 +0200

    Add type

--- src/prisma/migrations/20240404012832_extend_credential_fields/migration.sql
+++ src/prisma/migrations/20240404012832_extend_credential_fields/migration.sql
@@ -2,13 +2,15 @@
   Warnings:
 
   - You are about to drop the column `pageId` on the `Page` table. All the data in the column will be lost.
+  - Added the required column `credentialType` to the `PendingSafeTransaction` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "Page" DROP COLUMN "pageId";
 
 -- AlterTable
-ALTER TABLE "PendingSafeTransaction" ADD COLUMN     "rewardIds" TEXT[];
+ALTER TABLE "PendingSafeTransaction" ADD COLUMN     "credentialType" "AttestationType" NOT NULL,
+ADD COLUMN     "rewardIds" TEXT[];
 
 -- CreateIndex
 CREATE INDEX "PendingSafeTransaction_rewardIds_idx" ON "PendingSafeTransaction" USING GIN ("rewardIds");
