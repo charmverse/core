7c3345589ddeaf7daf2b1e835ee3356fc3f74281
mattcasey <mattwad@gmail.com>
2024-04-11 13:47:17 -0600

    add spaceId to forms

--- src/prisma/migrations/20240411194709_add_form_space_id/migration.sql
+++ src/prisma/migrations/20240411194709_add_form_space_id/migration.sql
@@ -1,5 +1,8 @@
 -- AlterTable
 ALTER TABLE "Form" ADD COLUMN     "spaceId" UUID;
 
+-- CreateIndex
+CREATE INDEX "Form_spaceId_idx" ON "Form"("spaceId");
+
 -- AddForeignKey
 ALTER TABLE "Form" ADD CONSTRAINT "Form_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
