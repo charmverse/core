b8fe202a6b11790961a369a36c673ab95cec1d8b
motechFR <mo@anyahq.co>
2024-04-16 12:54:54 +0200

    Extra cleanup

--- src/prisma/migrations/20240416105417_pages_lock/migration.sql
+++ src/prisma/migrations/20240416105417_pages_lock/migration.sql
@@ -1,11 +1,18 @@
 /*
   Warnings:
 
+  - The values [edit_isPublic] on the enum `PageOperations` will be removed. If these variants are still used in the database, this will fail.
   - The values [proposal_editor] on the enum `PagePermissionLevel` will be removed. If these variants are still used in the database, this will fail.
 
 */
 -- AlterEnum
-ALTER TYPE "PageOperations" ADD VALUE 'edit_lock';
+BEGIN;
+CREATE TYPE "PageOperations_new" AS ENUM ('read', 'delete', 'edit_position', 'edit_content', 'edit_path', 'grant_permissions', 'comment', 'create_poll', 'delete_attached_bounty', 'edit_lock');
+ALTER TABLE "PagePermission" ALTER COLUMN "permissions" TYPE "PageOperations_new"[] USING ("permissions"::text::"PageOperations_new"[]);
+ALTER TYPE "PageOperations" RENAME TO "PageOperations_old";
+ALTER TYPE "PageOperations_new" RENAME TO "PageOperations";
+DROP TYPE "PageOperations_old";
+COMMIT;
 
 -- AlterEnum
 BEGIN;
