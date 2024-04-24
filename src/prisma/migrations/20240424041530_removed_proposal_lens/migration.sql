5bf699fc63ae608bab95ea7707a94c19f36dc686
Safwan Shaheer <devorein00@gmail.com>
2024-04-24 10:55:46 +0600

    Removed lensPostLink column from proposal model (#281)
    * Removed lensPostLink column from proposal model
    
    * ci: version bump to 0.43.1-rc-feat-drop-propo.0
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20240424041530_removed_proposal_lens/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lensPostLink` on the `Proposal` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Proposal" DROP COLUMN "lensPostLink";
