9758906f60e4ce39afc7cb20856c855934e8f5df
Safwan Shaheer <devorein00@gmail.com>
2024-05-03 03:30:23 +0600

    Changed to actionLabels

--- src/prisma/migrations/20240430120827_proposal_evaluation_button_labels/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "ProposalEvaluation" ADD COLUMN     "actionButtonLabels" JSONB;
---
+++ src/prisma/migrations/20240502212938_proposal_evaluation_action_label/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ProposalEvaluation" ADD COLUMN     "actionLabels" JSONB;
