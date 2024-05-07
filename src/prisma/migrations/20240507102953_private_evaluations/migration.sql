0e8a2a7d0131b9d5e576de6b185da8b33e90a4be
motechFR <mo@anyahq.co>
2024-05-07 17:51:06 +0200

    Remove extra field

--- src/prisma/migrations/20240507102953_private_evaluations/migration.sql
+++ src/prisma/migrations/20240507102953_private_evaluations/migration.sql
@@ -1,5 +1,2 @@
--- AlterEnum
-ALTER TYPE "ProposalEvaluationType" ADD VALUE 'private_evaluation';
-
 -- AlterTable
 ALTER TABLE "ProposalWorkflow" ADD COLUMN     "privateEvaluations" BOOLEAN DEFAULT false;
