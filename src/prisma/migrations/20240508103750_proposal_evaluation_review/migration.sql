2a2940130ad979e91a3525039bc1cab98254a041
Safwan Shaheer <devorein00@gmail.com>
2024-05-08 16:37:59 +0600

    Proposal evaluation review

---
+++ src/prisma/migrations/20240508103750_proposal_evaluation_review/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "ProposalEvaluationReview" (
+    "id" UUID NOT NULL,
+    "completedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "result" "ProposalEvaluationResult" NOT NULL,
+    "reviewerId" UUID NOT NULL,
+    "evaluationId" UUID NOT NULL,
+
+    CONSTRAINT "ProposalEvaluationReview_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProposalEvaluationReview" ADD CONSTRAINT "ProposalEvaluationReview_reviewerId_fkey" FOREIGN KEY ("reviewerId") REFERENCES "ProposalReviewer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProposalEvaluationReview" ADD CONSTRAINT "ProposalEvaluationReview_evaluationId_fkey" FOREIGN KEY ("evaluationId") REFERENCES "ProposalEvaluation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
