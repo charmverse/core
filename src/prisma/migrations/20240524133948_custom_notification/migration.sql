747d4c340f390033bd262663310f2c40809b3622
Safwan Shaheer <devorein00@gmail.com>
2024-05-24 19:39:52 +0600

    Custom notification prisma model

---
+++ src/prisma/migrations/20240524133948_custom_notification/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "CustomNotification" (
+    "id" UUID NOT NULL,
+    "notificationMetadataId" UUID NOT NULL,
+    "type" TEXT NOT NULL,
+    "content" JSONB,
+
+    CONSTRAINT "CustomNotification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "CustomNotification" ADD CONSTRAINT "CustomNotification_notificationMetadataId_fkey" FOREIGN KEY ("notificationMetadataId") REFERENCES "UserNotificationMetadata"("id") ON DELETE CASCADE ON UPDATE CASCADE;
