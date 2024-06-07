2a7d3f9d89c0ce77fe99b92b44d8c7de020b5443
Safwan Shaheer <devorein00@gmail.com>
2024-06-07 07:07:56 +0600

    Added decline input to proposal appeal review

--- src/prisma/migrations/20240607010725_review_decline_input/migration.sql
+++ src/prisma/migrations/20240607010725_review_decline_input/migration.sql
@@ -1,2 +1,5 @@
 -- AlterTable
 ALTER TABLE "ProposalEvaluationAppealReview" ADD COLUMN     "declineInput" TEXT;
+
+-- AlterTable
+ALTER TABLE "ProposalEvaluationReview" ADD COLUMN     "declineInput" TEXT;
