de7ab5f5f91cb75aa7e6380dd6e47399747d3bf5
Safwan Shaheer <devorein00@gmail.com>
2024-06-25 13:21:43 +0600

    Added more connect fields

--- src/prisma/migrations/20240625072129_project_connect_fields/migration.sql
+++ src/prisma/migrations/20240625072129_project_connect_fields/migration.sql
@@ -1,7 +1,14 @@
+-- CreateEnum
+CREATE TYPE "ProjectSource" AS ENUM ('charmverse', 'connect');
+
 -- AlterTable
 ALTER TABLE "Project" ADD COLUMN     "avatar" TEXT,
 ADD COLUMN     "category" TEXT,
 ADD COLUMN     "coverImage" TEXT,
 ADD COLUMN     "farcasterIds" TEXT[] DEFAULT ARRAY[]::TEXT[],
 ADD COLUMN     "mirror" TEXT,
+ADD COLUMN     "source" "ProjectSource" NOT NULL DEFAULT 'charmverse',
 ADD COLUMN     "websites" TEXT[] DEFAULT ARRAY[]::TEXT[];
+
+-- AlterTable
+ALTER TABLE "ProjectMember" ADD COLUMN     "farcasterId" TEXT;
