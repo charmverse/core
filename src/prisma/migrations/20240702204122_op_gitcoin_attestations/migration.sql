a3635429d530f49f19782d875d7743132fb3120c
motechFR <mo@anyahq.co>
2024-07-02 22:41:40 +0200

    Cleanup migrations

--- src/prisma/migrations/20240702204122_op_gitcoin_attestations/migration.sql
+++ src/prisma/migrations/20240702204122_op_gitcoin_attestations/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+CREATE TABLE "OptimismProjectAttestation" (
+    "projectRefUID" TEXT NOT NULL,
+    "farcasterIds" TEXT[],
+    "name" TEXT NOT NULL,
+    "metadataAttestationUID" TEXT NOT NULL,
+    "metadataUrl" TEXT NOT NULL,
+    "metadata" JSONB NOT NULL,
+    "timeCreated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OptimismProjectAttestation_pkey" PRIMARY KEY ("projectRefUID")
+);
+
+-- CreateTable
 CREATE TABLE "GitcoinProjectAttestation" (
     "id" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
