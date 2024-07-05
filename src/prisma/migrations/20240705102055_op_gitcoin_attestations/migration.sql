141d21b12e932969ea028e6f8be8d1f8f1b497c7
Safwan Shaheer <devorein00@gmail.com>
2024-07-05 16:49:11 +0600

    Added optimism Project attestation model (#316)
    * Added optimism Project attestation model
    
    * ci: version bump to 0.66.1-rc-feat-eas-attest.0
    
    * Add gitcoin attestation table
    
    * ci: version bump to 0.66.1-rc-feat-eas-attest.1
    
    * Cleanup migrations
    
    * Fix project schemas
    
    * ci: version bump to 0.66.1-rc-feat-eas-attest.2
    
    * Fix migrations
    
    * ci: version bump to 0.66.1-rc-feat-eas-attest.3
    
    * Recreated prisma migration script
    
    * ci: version bump to 0.68.1-rc-feat-eas-attest.0
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
    Co-authored-by: motechFR <mo@anyahq.co>

---
+++ src/prisma/migrations/20240705102055_op_gitcoin_attestations/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "OptimismProjectAttestation" (
+    "projectRefUID" TEXT NOT NULL,
+    "farcasterIds" INTEGER[],
+    "chainId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "metadataAttestationUID" TEXT NOT NULL,
+    "metadataUrl" TEXT NOT NULL,
+    "metadata" JSONB NOT NULL,
+    "timeCreated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OptimismProjectAttestation_pkey" PRIMARY KEY ("projectRefUID")
+);
+
+-- CreateTable
+CREATE TABLE "GitcoinProjectAttestation" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "projectId" UUID NOT NULL,
+    "chainId" INTEGER NOT NULL,
+    "schemaId" TEXT NOT NULL,
+    "attestationUID" TEXT NOT NULL,
+
+    CONSTRAINT "GitcoinProjectAttestation_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "GitcoinProjectAttestation" ADD CONSTRAINT "GitcoinProjectAttestation_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
