2adefe4dd8d9b7c7dcffca6ff21f94e89049a093
motechFR <mo@anyahq.co>
2024-07-26 11:34:15 +0200

    Update migration

--- src/prisma/migrations/20240726093404_sunny_project_fields/migration.sql
+++ src/prisma/migrations/20240726093404_sunny_project_fields/migration.sql
@@ -5,4 +5,6 @@ CREATE TYPE "SunnyAwardsProjectType" AS ENUM ('creator', 'app', 'other');
 ALTER TABLE "Project" ADD COLUMN     "mintingWalletAddress" TEXT,
 ADD COLUMN     "primaryContractAddress" TEXT,
 ADD COLUMN     "primaryContractChainId" INTEGER,
+ADD COLUMN     "primaryContractDeployTxHash" TEXT,
+ADD COLUMN     "primaryContractDeployer" TEXT,
 ADD COLUMN     "sunnyAwardsProjectType" "SunnyAwardsProjectType";
