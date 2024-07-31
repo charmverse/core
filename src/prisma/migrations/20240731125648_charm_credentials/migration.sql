e2aad9fe8b8ba69b841941b268a64b43baf32fa1
motechFR <mo@anyahq.co>
2024-07-31 14:57:01 +0200

    Update models

--- src/prisma/migrations/20240731125648_charm_credentials/migration.sql
+++ src/prisma/migrations/20240731125648_charm_credentials/migration.sql
@@ -14,6 +14,22 @@ CREATE TABLE "CharmUserCredential" (
 );
 
 -- CreateTable
+CREATE TABLE "CharmProjectCredential" (
+    "projectRefUID" TEXT NOT NULL,
+    "farcasterIds" INTEGER[],
+    "chainId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "metadataAttestationUID" TEXT NOT NULL,
+    "metadataUrl" TEXT NOT NULL,
+    "metadata" JSONB NOT NULL,
+    "timeCreated" TIMESTAMP(3) NOT NULL,
+    "projectId" UUID,
+    "userId" UUID NOT NULL,
+
+    CONSTRAINT "CharmProjectCredential_pkey" PRIMARY KEY ("projectRefUID")
+);
+
+-- CreateTable
 CREATE TABLE "CharmQualifyingEventCredential" (
     "id" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -24,6 +40,7 @@ CREATE TABLE "CharmQualifyingEventCredential" (
     "userId" UUID NOT NULL,
     "resourceUrl" TEXT,
     "proposalId" UUID,
+    "projectId" UUID,
 
     CONSTRAINT "CharmQualifyingEventCredential_pkey" PRIMARY KEY ("id")
 );
@@ -32,7 +49,16 @@ CREATE TABLE "CharmQualifyingEventCredential" (
 ALTER TABLE "CharmUserCredential" ADD CONSTRAINT "CharmUserCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "CharmProjectCredential" ADD CONSTRAINT "CharmProjectCredential_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CharmProjectCredential" ADD CONSTRAINT "CharmProjectCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "CharmQualifyingEventCredential" ADD CONSTRAINT "CharmQualifyingEventCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "CharmQualifyingEventCredential" ADD CONSTRAINT "CharmQualifyingEventCredential_proposalId_fkey" FOREIGN KEY ("proposalId") REFERENCES "Proposal"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CharmQualifyingEventCredential" ADD CONSTRAINT "CharmQualifyingEventCredential_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
