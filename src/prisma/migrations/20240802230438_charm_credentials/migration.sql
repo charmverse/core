de8529ba6c3e8ee14f71a7310c2f67a2f7a85656
motechFR <mo@anyahq.co>
2024-08-03 01:05:18 +0200

    Setup Charm Credentials collection (#320)
    * Setup Charm Credentials collection
    
    * ci: version bump to 0.69.1-rc-feat-charm-cred.0
    
    * Cleanup migrations
    
    * ci: version bump to 0.69.1-rc-feat-charm-cred.1
    
    * ci: version bump to 0.70.4-rc-feat-charm-cred.0
    
    * ci: version bump to 0.72.2-rc-feat-charm-cred.0
    
    * Update models
    
    * ci: version bump to 0.72.2-rc-feat-charm-cred.1
    
    * Fix schema
    
    * ci: version bump to 0.72.2-rc-feat-charm-cred.2
    
    * ci: version bump to 0.72.3-rc-feat-charm-cred.0
    
    * Fix schema
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20240802230438_charm_credentials/migration.sql
@@ -0,0 +1,73 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `makeRewardsPublic` on the `Proposal` table. All the data in the column will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "QualifyingEventType" AS ENUM ('proposal_published', 'pull_request_merged');
+
+-- AlterTable -- This column should never have been added
+ALTER TABLE "Proposal" DROP COLUMN "makeRewardsPublic";
+
+-- CreateTable
+CREATE TABLE "CharmUserCredential" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "chainId" INTEGER NOT NULL,
+    "schemaId" TEXT NOT NULL,
+    "attestationUID" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+
+    CONSTRAINT "CharmUserCredential_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CharmProjectCredential" (
+    "projectRefUID" TEXT NOT NULL,
+    "farcasterIds" INTEGER[],
+    "chainId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "metadataAttestationUID" TEXT NOT NULL,
+    "metadataUrl" TEXT NOT NULL,
+    "metadata" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "projectId" UUID,
+    "userId" UUID NOT NULL,
+
+    CONSTRAINT "CharmProjectCredential_pkey" PRIMARY KEY ("projectRefUID")
+);
+
+-- CreateTable
+CREATE TABLE "CharmQualifyingEventCredential" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "qualifyingEventType" "QualifyingEventType" NOT NULL,
+    "chainId" INTEGER NOT NULL,
+    "schemaId" TEXT NOT NULL,
+    "attestationUID" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+    "resourceUrl" TEXT,
+    "proposalId" UUID,
+    "projectId" UUID,
+
+    CONSTRAINT "CharmQualifyingEventCredential_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "CharmUserCredential" ADD CONSTRAINT "CharmUserCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CharmProjectCredential" ADD CONSTRAINT "CharmProjectCredential_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CharmProjectCredential" ADD CONSTRAINT "CharmProjectCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CharmQualifyingEventCredential" ADD CONSTRAINT "CharmQualifyingEventCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CharmQualifyingEventCredential" ADD CONSTRAINT "CharmQualifyingEventCredential_proposalId_fkey" FOREIGN KEY ("proposalId") REFERENCES "Proposal"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CharmQualifyingEventCredential" ADD CONSTRAINT "CharmQualifyingEventCredential_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
