6cd0a61488b91276e47627e534edc39b6c346b3e
Safwan Shaheer <devorein00@gmail.com>
2024-08-10 01:07:48 +0600

    Composer action frame model (#331)
    * Composer action frame model
    
    * ci: version bump to 0.74.2-rc-feat-apps-farca.0
    
    * ci: version bump to 0.74.2-rc-feat-apps-farca.1
    
    * updated columns and table name
    
    * ci: version bump to 0.74.2-rc-feat-apps-farca.2
    
    * Remove author fid from farcaster frame model
    
    * ci: version bump to 0.74.2-rc-feat-apps-farca.3
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20240809185010_product_updates_frame/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "ProductUpdatesFarcasterFrame" (
+    "id" UUID NOT NULL,
+    "text" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdBy" UUID NOT NULL,
+    "projectId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAtLocal" TEXT NOT NULL,
+
+    CONSTRAINT "ProductUpdatesFarcasterFrame_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProductUpdatesFarcasterFrame" ADD CONSTRAINT "ProductUpdatesFarcasterFrame_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductUpdatesFarcasterFrame" ADD CONSTRAINT "ProductUpdatesFarcasterFrame_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
