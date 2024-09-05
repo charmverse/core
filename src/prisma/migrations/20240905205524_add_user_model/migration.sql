6299ee08f83de82ff39f3d8979c6afc40e1d2fea
Matt Casey <mattwad@gmail.com>
2024-09-05 16:09:08 -0500

    add scout user model (#350)
    * add scout user model
    
    * ci: version bump to 0.81.3-rc-scoutgame.0
    
    * ci: version bump to 0.81.4-rc-scoutgame.0
    
    * add onboarding fields
    
    * ci: version bump to 0.81.4-rc-scoutgame.1
    
    * update schema one more time
    
    * ci: version bump to 0.81.4-rc-scoutgame.2
    
    * update schema again
    
    * ci: version bump to 0.81.4-rc-scoutgame.3
    
    * int for farcaster id
    
    * ci: version bump to 0.81.4-rc-scoutgame.4
    
    * add indices
    
    * ci: version bump to 0.81.4-rc-scoutgame.5
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
    Co-authored-by: Safwan Shaheer <devorein00@gmail.com>

---
+++ src/prisma/migrations/20240905205524_add_user_model/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "Scout" (
+    "id" UUID NOT NULL,
+    "email" TEXT,
+    "username" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+    "farcasterId" INTEGER,
+    "farcasterName" TEXT,
+    "walletAddress" TEXT,
+    "walletENS" TEXT,
+    "avatar" TEXT,
+    "bio" TEXT,
+    "builder" BOOLEAN NOT NULL DEFAULT false,
+    "sendMarketing" BOOLEAN NOT NULL DEFAULT false,
+    "agreedToTOS" BOOLEAN NOT NULL DEFAULT false,
+    "onboarded" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Scout_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Scout_username_key" ON "Scout"("username");
+
+-- CreateIndex
+CREATE INDEX "Scout_username_idx" ON "Scout"("username");
+
+-- CreateIndex
+CREATE INDEX "Scout_farcasterId_idx" ON "Scout"("farcasterId");
+
+-- CreateIndex
+CREATE INDEX "Scout_walletAddress_idx" ON "Scout"("walletAddress");
