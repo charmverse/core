5200dc292c5c965f18e745a575e3168d1264780c
mattcasey <mattwad@gmail.com>
2024-09-05 11:38:30 -0600

    update schema one more time

--- src/prisma/migrations/20240905171933_add_scout_user_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Scout" ADD COLUMN     "agreedToTOS" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "onboarded" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "sendMarketing" BOOLEAN NOT NULL DEFAULT false;
---
+++ src/prisma/migrations/20240905173817_add_scout_user_model/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "Scout" (
+    "id" UUID NOT NULL,
+    "email" TEXT,
+    "username" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "farcasterId" TEXT,
+    "farcasterName" TEXT,
+    "walletAddress" TEXT,
+    "walletENS" TEXT,
+    "avatar" TEXT,
+    "builder" BOOLEAN NOT NULL DEFAULT false,
+    "sendMarketing" BOOLEAN NOT NULL DEFAULT false,
+    "agreedToTOS" BOOLEAN NOT NULL DEFAULT false,
+    "onboarded" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Scout_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Scout_username_key" ON "Scout"("username");
+
+-- CreateIndex
+CREATE INDEX "Scout_username_idx" ON "Scout"("username");
