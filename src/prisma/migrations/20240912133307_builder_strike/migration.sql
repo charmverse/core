5e27a4744cf8a9bfddcec9b7c17df464525abb7a
Safwan Shaheer <devorein00@gmail.com>
2024-09-12 19:33:19 +0600

    Added github related models

---
+++ src/prisma/migrations/20240912133307_builder_strike/migration.sql
@@ -0,0 +1,76 @@
+-- CreateEnum
+CREATE TYPE "GithubEventType" AS ENUM ('merged_pull_request');
+
+-- AlterTable
+ALTER TABLE "Scout" ADD COLUMN     "bannedAt" TIMESTAMP(3);
+
+-- CreateTable
+CREATE TABLE "BuilderStrike" (
+    "id" UUID NOT NULL,
+    "builderId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "builderEventId" UUID NOT NULL,
+
+    CONSTRAINT "BuilderStrike_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BuilderEvent" (
+    "id" UUID NOT NULL,
+    "builderId" UUID NOT NULL,
+    "season" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "BuilderEvent_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GithubUser" (
+    "id" INTEGER NOT NULL,
+    "builderId" UUID,
+    "email" TEXT,
+    "displayName" TEXT,
+    "login" TEXT NOT NULL,
+
+    CONSTRAINT "GithubUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GithubRepo" (
+    "id" TEXT NOT NULL,
+    "owner" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "GithubRepo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GithubEvent" (
+    "id" TEXT NOT NULL,
+    "repoId" TEXT NOT NULL,
+    "pullRequestNumber" INTEGER NOT NULL,
+    "type" "GithubEventType" NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdBy" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "GithubEvent_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "BuilderStrike" ADD CONSTRAINT "BuilderStrike_builderId_fkey" FOREIGN KEY ("builderId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BuilderStrike" ADD CONSTRAINT "BuilderStrike_builderEventId_fkey" FOREIGN KEY ("builderEventId") REFERENCES "BuilderEvent"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BuilderEvent" ADD CONSTRAINT "BuilderEvent_builderId_fkey" FOREIGN KEY ("builderId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GithubUser" ADD CONSTRAINT "GithubUser_builderId_fkey" FOREIGN KEY ("builderId") REFERENCES "Scout"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GithubEvent" ADD CONSTRAINT "GithubEvent_repoId_fkey" FOREIGN KEY ("repoId") REFERENCES "GithubRepo"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GithubEvent" ADD CONSTRAINT "GithubEvent_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "GithubUser"("id") ON DELETE CASCADE ON UPDATE CASCADE;
