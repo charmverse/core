b987cf7de7876ca88ff598ab09513899d910d0b3
mattcasey <mattwad@gmail.com>
2024-09-26 14:49:28 -0600

    commit migration

---
+++ src/prisma/migrations/20240926204918_update_season_type/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "BuilderEvent" ALTER COLUMN "season" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "BuilderNft" ALTER COLUMN "season" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "UserSeasonStats" ALTER COLUMN "season" SET DATA TYPE TEXT;
