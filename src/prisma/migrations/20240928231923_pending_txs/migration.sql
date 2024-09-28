7890ebc4c744cdf72fc66f5f0c8f8631b0a52264
motechFR <mo@anyahq.co>
2024-09-29 01:19:40 +0200

    Add pending transactions

---
+++ src/prisma/migrations/20240928231923_pending_txs/migration.sql
@@ -0,0 +1,37 @@
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('pending', 'completed', 'failed');
+
+-- CreateTable
+CREATE TABLE "PendingNftTransaction" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" UUID NOT NULL,
+    "sourceAmountIn" BIGINT,
+    "sourceCurrency" TEXT NOT NULL,
+    "targetAmountOut" BIGINT NOT NULL,
+    "targetCurrency" TEXT NOT NULL,
+    "sourceChainTxHash" TEXT NOT NULL,
+    "sourceChainId" INTEGER NOT NULL,
+    "destinationChainTxHash" TEXT,
+    "destinationChainId" INTEGER,
+    "recipientAddress" TEXT NOT NULL,
+    "senderAddress" TEXT NOT NULL,
+    "status" "TransactionStatus" NOT NULL DEFAULT 'pending',
+
+    CONSTRAINT "PendingNftTransaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "PendingNftTransaction_userId_idx" ON "PendingNftTransaction"("userId");
+
+-- CreateIndex
+CREATE INDEX "PendingNftTransaction_status_idx" ON "PendingNftTransaction"("status");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PendingNftTransaction_sourceChainTxHash_sourceChainId_key" ON "PendingNftTransaction"("sourceChainTxHash", "sourceChainId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PendingNftTransaction_destinationChainTxHash_destinationCha_key" ON "PendingNftTransaction"("destinationChainTxHash", "destinationChainId");
+
+-- AddForeignKey
+ALTER TABLE "PendingNftTransaction" ADD CONSTRAINT "PendingNftTransaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
