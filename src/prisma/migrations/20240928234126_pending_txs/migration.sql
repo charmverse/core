64fb93676470ea1dc08488647d52a6cfb7cb2bdf
motechFR <mo@anyahq.co>
2024-09-29 04:24:41 +0200

    Add pending transactions (#364)
    * Add pending transactions
    
    * ci: version bump to 0.86.1-rc-pending-txs.0
    
    * Improve schema
    
    * ci: version bump to 0.86.1-rc-pending-txs.1
    
    * Always include destination chain id
    
    * ci: version bump to 0.86.1-rc-pending-txs.2
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20240928234126_pending_txs/migration.sql
@@ -0,0 +1,37 @@
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('pending', 'completed', 'failed');
+
+-- CreateTable
+CREATE TABLE "PendingNftTransaction" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" UUID NOT NULL,
+    "sourceChainTxHash" TEXT NOT NULL,
+    "sourceChainId" INTEGER NOT NULL,
+    "targetAmountReceived" BIGINT NOT NULL,
+    "targetCurrencyContractAddress" TEXT NOT NULL,
+    "destinationChainId" INTEGER NOT NULL,
+    "destinationChainTxHash" TEXT,
+    "contractAddress" TEXT NOT NULL,
+    "tokenId" BIGINT NOT NULL,
+    "tokenAmount" INTEGER NOT NULL,
+    "senderAddress" TEXT NOT NULL,
+    "status" "TransactionStatus" NOT NULL DEFAULT 'pending',
+
+    CONSTRAINT "PendingNftTransaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "PendingNftTransaction_userId_idx" ON "PendingNftTransaction"("userId");
+
+-- CreateIndex
+CREATE INDEX "PendingNftTransaction_status_idx" ON "PendingNftTransaction"("status");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PendingNftTransaction_sourceChainTxHash_sourceChainId_key" ON "PendingNftTransaction"("sourceChainTxHash", "sourceChainId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PendingNftTransaction_destinationChainTxHash_destinationCha_key" ON "PendingNftTransaction"("destinationChainTxHash", "destinationChainId");
+
+-- AddForeignKey
+ALTER TABLE "PendingNftTransaction" ADD CONSTRAINT "PendingNftTransaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
