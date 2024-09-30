bc37c758e3f7c0bb0c21fa06713a7599917c0f9f
Safwan Shaheer <devorein00@gmail.com>
2024-10-01 04:54:16 +0600

    .

--- src/prisma/migrations/20240930225407_sg_activity/migration.sql
+++ src/prisma/migrations/20240930225407_sg_activity/migration.sql
@@ -3,19 +3,20 @@
 
   - The values [mint,gems_from_pr,gems,strike,builder_registered] on the enum `ScoutGameActivityType` will be removed. If these variants are still used in the database, this will fail.
   - You are about to drop the column `gemsPayoutEventId` on the `ScoutGameActivity` table. All the data in the column will be lost.
+  - You are about to drop the column `notificationSentAt` on the `ScoutGameActivity` table. All the data in the column will be lost.
   - You are about to drop the column `onchainChainId` on the `ScoutGameActivity` table. All the data in the column will be lost.
   - You are about to drop the column `onchainTxHash` on the `ScoutGameActivity` table. All the data in the column will be lost.
   - You are about to drop the column `pointsDirection` on the `ScoutGameActivity` table. All the data in the column will be lost.
   - You are about to drop the column `registeredBuilderNftId` on the `ScoutGameActivity` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[userId,pointsReceiptId]` on the table `ScoutGameActivity` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[userId,nftPurchaseEventId]` on the table `ScoutGameActivity` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[userId,builderStrikeId]` on the table `ScoutGameActivity` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[userId,gemsReceiptId]` on the table `ScoutGameActivity` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[userId,recipientType,pointsReceiptId]` on the table `ScoutGameActivity` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[userId,recipientType,nftPurchaseEventId]` on the table `ScoutGameActivity` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[userId,recipientType,builderStrikeId]` on the table `ScoutGameActivity` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[userId,recipientType,gemsReceiptId]` on the table `ScoutGameActivity` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `recipientType` to the `ScoutGameActivity` table without a default value. This is not possible if the table is not empty.
 
 */
 -- CreateEnum
-CREATE TYPE "ActivityReceipientType" AS ENUM ('builder', 'scout');
+CREATE TYPE "ActivityRecipientType" AS ENUM ('builder', 'scout');
 
 -- AlterEnum
 BEGIN;
@@ -34,6 +35,9 @@ ALTER TABLE "ScoutGameActivity" DROP CONSTRAINT "ScoutGameActivity_registeredBui
 
 -- DropIndex
 DROP INDEX "ScoutGameActivity_gemsPayoutEventId_idx";
+
+-- DropIndex
+DROP INDEX "ScoutGameActivity_notificationSentAt_idx";
 
 -- DropIndex
 DROP INDEX "ScoutGameActivity_onchainChainId_idx";
@@ -61,23 +65,24 @@ DROP INDEX "ScoutGameActivity_userId_pointsDirection_registeredBuilderN_key";
 
 -- AlterTable
 ALTER TABLE "ScoutGameActivity" DROP COLUMN "gemsPayoutEventId",
+DROP COLUMN "notificationSentAt",
 DROP COLUMN "onchainChainId",
 DROP COLUMN "onchainTxHash",
 DROP COLUMN "pointsDirection",
 DROP COLUMN "registeredBuilderNftId",
-ADD COLUMN     "recipientType" "ActivityReceipientType" NOT NULL;
+ADD COLUMN     "recipientType" "ActivityRecipientType" NOT NULL;
 
 -- DropEnum
 DROP TYPE "PointsDirection";
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ScoutGameActivity_userId_pointsReceiptId_key" ON "ScoutGameActivity"("userId", "pointsReceiptId");
+CREATE UNIQUE INDEX "ScoutGameActivity_userId_recipientType_pointsReceiptId_key" ON "ScoutGameActivity"("userId", "recipientType", "pointsReceiptId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ScoutGameActivity_userId_nftPurchaseEventId_key" ON "ScoutGameActivity"("userId", "nftPurchaseEventId");
+CREATE UNIQUE INDEX "ScoutGameActivity_userId_recipientType_nftPurchaseEventId_key" ON "ScoutGameActivity"("userId", "recipientType", "nftPurchaseEventId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ScoutGameActivity_userId_builderStrikeId_key" ON "ScoutGameActivity"("userId", "builderStrikeId");
+CREATE UNIQUE INDEX "ScoutGameActivity_userId_recipientType_builderStrikeId_key" ON "ScoutGameActivity"("userId", "recipientType", "builderStrikeId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ScoutGameActivity_userId_gemsReceiptId_key" ON "ScoutGameActivity"("userId", "gemsReceiptId");
+CREATE UNIQUE INDEX "ScoutGameActivity_userId_recipientType_gemsReceiptId_key" ON "ScoutGameActivity"("userId", "recipientType", "gemsReceiptId");
