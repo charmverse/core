ecc3516807e114f913565935f9da2210bb8a82f0
motechFR <mo@anyahq.co>
2024-10-24 14:52:04 +0300

    Fix migration

--- src/prisma/migrations/20241024114719_add_evaluation_settings/migration.sql
+++ src/prisma/migrations/20241024114719_add_evaluation_settings/migration.sql
@@ -1,6 +1,3 @@
--- AlterEnum
-ALTER TYPE "ProposalOperation" ADD VALUE 'view_evaluations';
-
 -- AlterTable
 ALTER TABLE "ProposalEvaluation" ADD COLUMN     "publishCommentsAndScores" BOOLEAN,
 ADD COLUMN     "showAuthorResultsOnRubricFail" BOOLEAN;
