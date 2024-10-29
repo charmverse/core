de63f65a505fe9353aca8ab99e4abf605f35b265
Valentin L <ludu.valentin@gmail.com>
2024-10-29 15:57:59 +0200

    Remove Id and add index

--- src/prisma/migrations/20241029135651_scout_wallet_addresses/migration.sql
+++ src/prisma/migrations/20241029135651_scout_wallet_addresses/migration.sql
@@ -1,15 +1,15 @@
 -- CreateTable
 CREATE TABLE "ScoutWallet" (
-    "id" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "address" TEXT NOT NULL,
-    "scoutId" UUID NOT NULL,
-
-    CONSTRAINT "ScoutWallet_pkey" PRIMARY KEY ("id")
+    "scoutId" UUID NOT NULL
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ScoutWallet_address_key" ON "ScoutWallet"("address");
+
+-- CreateIndex
+CREATE INDEX "ScoutWallet_address_idx" ON "ScoutWallet"("address");
 
 -- AddForeignKey
 ALTER TABLE "ScoutWallet" ADD CONSTRAINT "ScoutWallet_scoutId_fkey" FOREIGN KEY ("scoutId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
