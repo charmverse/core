95f5b2895d4444de10497be82f1778c00a575af8
Safwan Shaheer <devorein00@gmail.com>
2024-10-31 18:29:08 +0600

    Dropped farcaster name field

--- src/prisma/migrations/20241031122855_farcaster_username/migration.sql
+++ src/prisma/migrations/20241031122855_farcaster_username/migration.sql
@@ -1,11 +1,13 @@
 /*
   Warnings:
 
+  - You are about to drop the column `farcasterName` on the `Scout` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[farcasterUsername]` on the table `Scout` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- AlterTable
-ALTER TABLE "Scout" ADD COLUMN     "farcasterUsername" TEXT;
+ALTER TABLE "Scout" DROP COLUMN "farcasterName",
+ADD COLUMN     "farcasterUsername" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Scout_farcasterUsername_key" ON "Scout"("farcasterUsername");
