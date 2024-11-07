3df2426c787c32c3136cde3c2eeb740b0e7bdfb7
motechFR <mo@anyahq.co>
2024-11-07 15:37:11 +0200

    Fix encoder for name schema

--- src/lib/protocol/easSchemas/constants.ts
+++ src/lib/protocol/easSchemas/constants.ts
@@ -6,7 +6,9 @@ export const NULL_EVM_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 // This allows us to encode the schemaId and name of a name schema attestation
 // Obtained from https://github.com/ethereum-attestation-service/eas-contracts/blob/558250dae4cb434859b1ac3b6d32833c6448be21/deploy/scripts/000004-name-initial-schemas.ts#L10C1-L11C1
-export const NAME_SCHEMA_UID = getSchemaUID('bytes32 schemaId,string name', NULL_EVM_ADDRESS, true) as `0x${string}`;
+export const NAME_SCHEMA_DEFINITION = 'bytes32 schemaId,string name';
+
+export const NAME_SCHEMA_UID = getSchemaUID(NAME_SCHEMA_DEFINITION, NULL_EVM_ADDRESS, true) as `0x${string}`;
 
 export type NameSchemaAttestation = {
   schemaId: `0x${string}`;
@@ -14,7 +16,7 @@ export type NameSchemaAttestation = {
 };
 
 export function encodeNameSchemaAttestation({ name, schemaId }: NameSchemaAttestation): `0x${string}` {
-  const encoder = new SchemaEncoder(NAME_SCHEMA_UID);
+  const encoder = new SchemaEncoder(NAME_SCHEMA_DEFINITION);
 
   return encoder.encodeData([
     { name: 'schemaId', type: 'bytes32', value: schemaId },
