88874f57947db0a01c1af77a720947250badbc65
Safwan Shaheer <devorein00@gmail.com>
2024-11-07 17:20:53 +0600

    Added social quest builder event type

--- src/prisma/migrations/20241107112044_scout_social_quest/migration.sql
+++ src/prisma/migrations/20241107112044_scout_social_quest/migration.sql
@@ -1,5 +1,13 @@
 -- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
 ALTER TYPE "BuilderEventType" ADD VALUE 'daily_claim';
+ALTER TYPE "BuilderEventType" ADD VALUE 'social_quest';
 
 -- AlterTable
 ALTER TABLE "Scout" ALTER COLUMN "telegramId" SET DATA TYPE BIGINT;
