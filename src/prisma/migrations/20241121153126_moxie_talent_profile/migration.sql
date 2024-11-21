1447f2e7b234616ccb2e054cb658af940bb81839
Safwan Shaheer <devorein00@gmail.com>
2024-11-21 21:32:12 +0600

    Removed moxie profile and use moxie boolean

--- src/prisma/migrations/20241121153126_moxie_talent_profile/migration.sql
+++ src/prisma/migrations/20241121153126_moxie_talent_profile/migration.sql
@@ -1,3 +1,6 @@
+-- AlterTable
+ALTER TABLE "Scout" ADD COLUMN     "hasMoxieProfile" BOOLEAN NOT NULL DEFAULT false;
+
 -- CreateTable
 CREATE TABLE "TalentProfile" (
     "id" INTEGER NOT NULL,
@@ -7,28 +10,11 @@ CREATE TABLE "TalentProfile" (
     CONSTRAINT "TalentProfile_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "MoxieProfile" (
-    "id" INTEGER NOT NULL,
-    "builderId" UUID NOT NULL,
-
-    CONSTRAINT "MoxieProfile_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "TalentProfile_builderId_key" ON "TalentProfile"("builderId");
 
 -- CreateIndex
 CREATE INDEX "TalentProfile_builderId_idx" ON "TalentProfile"("builderId");
 
--- CreateIndex
-CREATE UNIQUE INDEX "MoxieProfile_builderId_key" ON "MoxieProfile"("builderId");
-
--- CreateIndex
-CREATE INDEX "MoxieProfile_builderId_idx" ON "MoxieProfile"("builderId");
-
 -- AddForeignKey
 ALTER TABLE "TalentProfile" ADD CONSTRAINT "TalentProfile_builderId_fkey" FOREIGN KEY ("builderId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "MoxieProfile" ADD CONSTRAINT "MoxieProfile_builderId_fkey" FOREIGN KEY ("builderId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
