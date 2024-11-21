c61f1d874c6bebd3d5e5498088cf36cc9848a118
Safwan Shaheer <devorein00@gmail.com>
2024-11-21 20:07:30 +0600

    Added moxie and talent profile models

---
+++ src/prisma/migrations/20241121140531_moxie_talent_profile/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "TalentProfile" (
+    "id" INTEGER NOT NULL,
+    "score" INTEGER NOT NULL,
+    "builderId" UUID NOT NULL,
+
+    CONSTRAINT "TalentProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MoxieProfile" (
+    "id" INTEGER NOT NULL,
+    "builderId" UUID NOT NULL,
+
+    CONSTRAINT "MoxieProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TalentProfile_builderId_key" ON "TalentProfile"("builderId");
+
+-- CreateIndex
+CREATE INDEX "TalentProfile_builderId_idx" ON "TalentProfile"("builderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MoxieProfile_builderId_key" ON "MoxieProfile"("builderId");
+
+-- CreateIndex
+CREATE INDEX "MoxieProfile_builderId_idx" ON "MoxieProfile"("builderId");
+
+-- AddForeignKey
+ALTER TABLE "TalentProfile" ADD CONSTRAINT "TalentProfile_builderId_fkey" FOREIGN KEY ("builderId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MoxieProfile" ADD CONSTRAINT "MoxieProfile_builderId_fkey" FOREIGN KEY ("builderId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
