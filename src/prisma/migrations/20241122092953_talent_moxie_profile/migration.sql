5721f2d29ebbfafa6e1d0e76271106f1eda30c4c
Safwan Shaheer <devorein00@gmail.com>
2024-11-22 15:30:29 +0600

    Added updated at and address for talent profile

--- src/prisma/migrations/20241122092953_talent_moxie_profile/migration.sql
+++ src/prisma/migrations/20241122092953_talent_moxie_profile/migration.sql
@@ -6,12 +6,17 @@ CREATE TABLE "TalentProfile" (
     "id" INTEGER NOT NULL,
     "score" INTEGER NOT NULL,
     "builderId" UUID NOT NULL,
+    "address" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "TalentProfile_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "TalentProfile_builderId_key" ON "TalentProfile"("builderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TalentProfile_address_key" ON "TalentProfile"("address");
 
 -- CreateIndex
 CREATE INDEX "TalentProfile_builderId_idx" ON "TalentProfile"("builderId");
