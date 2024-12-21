a2c665d0c72e0d2eaeea785bcc36f048882c1763
motechFR <mo@anyahq.co>
2024-12-21 07:33:34 +0200

    Add onchain models (#399)
    * Add onchain models
    
    * ci: version bump to 0.98.7-rc-onchain-token-m.0
    
    * Tweak the events
    
    * ci: version bump to 0.98.7-rc-onchain-token-m.1
    
    * Add missing unique
    
    * ci: version bump to 0.98.7-rc-onchain-token-m.2
    
    * Cleanup relations
    
    * ci: version bump to 0.98.7-rc-onchain-token-m.3
    
    * ci: version bump to 0.98.8-rc-onchain-token-m.0
    
    * Extend referralplatform
    
    * ci: version bump to 0.98.8-rc-onchain-token-m.1
    
    * ci: version bump to 0.98.9-rc-onchain-token-m.0
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20241220110027_onchain_models/migration.sql
@@ -0,0 +1,73 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `recipientId` on the `TokensReceipt` table. All the data in the column will be lost.
+  - You are about to drop the column `senderId` on the `TokensReceipt` table. All the data in the column will be lost.
+
+*/
+-- AlterEnum
+ALTER TYPE "ReferralPlatform" ADD VALUE 'onchain_webapp';
+
+-- DropForeignKey
+ALTER TABLE "BuilderEvent" DROP CONSTRAINT "BuilderEvent_weeklyClaimId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TokensReceipt" DROP CONSTRAINT "TokensReceipt_recipientId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TokensReceipt" DROP CONSTRAINT "TokensReceipt_senderId_fkey";
+
+-- DropIndex
+DROP INDEX "TokensReceipt_recipientId_idx";
+
+-- DropIndex
+DROP INDEX "TokensReceipt_senderId_idx";
+
+-- AlterTable
+ALTER TABLE "TokensReceipt" DROP COLUMN "recipientId",
+DROP COLUMN "senderId",
+ADD COLUMN     "claimTxHash" TEXT,
+ADD COLUMN     "recipientWalletAddress" TEXT,
+ADD COLUMN     "senderWalletAddress" TEXT;
+
+-- CreateTable
+CREATE TABLE "ScoutNft" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "builderNftId" UUID NOT NULL,
+    "walletAddress" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
+
+    CONSTRAINT "ScoutNft_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "ScoutNft_builderNftId_idx" ON "ScoutNft"("builderNftId");
+
+-- CreateIndex
+CREATE INDEX "ScoutNft_walletAddress_idx" ON "ScoutNft"("walletAddress");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ScoutNft_builderNftId_walletAddress_key" ON "ScoutNft"("builderNftId", "walletAddress");
+
+-- CreateIndex
+CREATE INDEX "TokensReceipt_recipientWalletAddress_idx" ON "TokensReceipt"("recipientWalletAddress");
+
+-- CreateIndex
+CREATE INDEX "TokensReceipt_senderWalletAddress_idx" ON "TokensReceipt"("senderWalletAddress");
+
+-- AddForeignKey
+ALTER TABLE "BuilderEvent" ADD CONSTRAINT "BuilderEvent_weeklyClaimId_fkey" FOREIGN KEY ("weeklyClaimId") REFERENCES "WeeklyClaims"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TokensReceipt" ADD CONSTRAINT "TokensReceipt_recipientWalletAddress_fkey" FOREIGN KEY ("recipientWalletAddress") REFERENCES "ScoutWallet"("address") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TokensReceipt" ADD CONSTRAINT "TokensReceipt_senderWalletAddress_fkey" FOREIGN KEY ("senderWalletAddress") REFERENCES "ScoutWallet"("address") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ScoutNft" ADD CONSTRAINT "ScoutNft_builderNftId_fkey" FOREIGN KEY ("builderNftId") REFERENCES "BuilderNft"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ScoutNft" ADD CONSTRAINT "ScoutNft_walletAddress_fkey" FOREIGN KEY ("walletAddress") REFERENCES "ScoutWallet"("address") ON DELETE CASCADE ON UPDATE CASCADE;
