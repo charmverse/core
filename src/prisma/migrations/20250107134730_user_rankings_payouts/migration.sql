ecb3603a972870232fdb2c9942cf48a2824a4c06
motechFR <mo@anyahq.co>
2025-01-07 16:52:30 +0100

    Adjust scoring columns

--- src/prisma/migrations/20250107134730_user_rankings_payouts/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "UserSeasonStats" ADD COLUMN     "estimatedPayout" INTEGER,
-ADD COLUMN     "level" INTEGER;
---
+++ src/prisma/migrations/20250107155205_user_rankings_payouts/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "BuilderNft" ADD COLUMN     "estimatedPayout" INTEGER;
+
+-- AlterTable
+ALTER TABLE "UserSeasonStats" ADD COLUMN     "level" INTEGER;
