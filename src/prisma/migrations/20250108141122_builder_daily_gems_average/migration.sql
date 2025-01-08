9450c364985959a9a4a9e03f98bc5f0300046e19
Safwan Shaheer <devorein00@gmail.com>
2025-01-08 20:11:42 +0600

    Added last 14 days gems could for builder card

--- src/prisma/migrations/20250108141122_builder_daily_gems_average/migration.sql
+++ src/prisma/migrations/20250108141122_builder_daily_gems_average/migration.sql
@@ -1,3 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `last14Days` to the `BuilderCardActivity` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "BuilderCardActivity" ADD COLUMN     "last14Days" JSONB NOT NULL;
+
 -- CreateTable
 CREATE TABLE "BuilderDailyGemsAverage" (
     "id" UUID NOT NULL,
