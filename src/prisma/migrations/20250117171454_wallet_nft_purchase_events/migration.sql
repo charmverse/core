041afc79669603262bfaf2ee4f7a6a0c234ea772
motechFR <mo@anyahq.co>
2025-01-17 18:15:14 +0100

    Update NFTPurchaseEvents

--- src/prisma/migrations/20250117171454_wallet_nft_purchase_events/migration.sql
+++ src/prisma/migrations/20250117171454_wallet_nft_purchase_events/migration.sql
@@ -1,5 +1,6 @@
 -- AlterTable
-ALTER TABLE "NFTPurchaseEvent" ADD COLUMN     "senderWalletAddress" TEXT;
+ALTER TABLE "NFTPurchaseEvent" ADD COLUMN     "senderWalletAddress" TEXT,
+ALTER COLUMN "scoutId" DROP NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE "NFTPurchaseEvent" ADD CONSTRAINT "NFTPurchaseEvent_senderWalletAddress_fkey" FOREIGN KEY ("senderWalletAddress") REFERENCES "ScoutWallet"("address") ON DELETE CASCADE ON UPDATE CASCADE;
