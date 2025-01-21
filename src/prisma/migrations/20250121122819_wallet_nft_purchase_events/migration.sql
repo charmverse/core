a440a54cc108f461305db732622e13226d77ea9e
motechFR <mo@anyahq.co>
2025-01-21 13:28:38 +0100

    Revert to mandatory scoutId to fix types

--- src/prisma/migrations/20250121122819_wallet_nft_purchase_events/migration.sql
+++ src/prisma/migrations/20250121122819_wallet_nft_purchase_events/migration.sql
@@ -1,6 +1,5 @@
 -- AlterTable
-ALTER TABLE "NFTPurchaseEvent" ADD COLUMN     "senderWalletAddress" TEXT,
-ALTER COLUMN "scoutId" DROP NOT NULL;
+ALTER TABLE "NFTPurchaseEvent" ADD COLUMN     "senderWalletAddress" TEXT;
 
 -- AddForeignKey
 ALTER TABLE "NFTPurchaseEvent" ADD CONSTRAINT "NFTPurchaseEvent_senderWalletAddress_fkey" FOREIGN KEY ("senderWalletAddress") REFERENCES "ScoutWallet"("address") ON DELETE CASCADE ON UPDATE CASCADE;
