1a611295a00f9f8eecb03def2e6ccb4a5e292b9b
mattcasey <mattwad@gmail.com>
2025-01-29 15:27:13 -0700

    update schema

---
+++ src/prisma/migrations/20250129222703_add_contract_txs/migration.sql
@@ -0,0 +1,61 @@
+-- AlterTable
+ALTER TABLE "ScoutProject" ALTER COLUMN "avatar" SET DEFAULT '',
+ALTER COLUMN "description" SET DEFAULT '',
+ALTER COLUMN "website" SET DEFAULT '',
+ALTER COLUMN "github" SET DEFAULT '',
+ALTER COLUMN "path" SET DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "ScoutProjectContract" ALTER COLUMN "blockNumber" SET DATA TYPE BIGINT;
+
+-- CreateTable
+CREATE TABLE "ScoutProjectContractTransaction" (
+    "contractId" UUID NOT NULL,
+    "from" TEXT NOT NULL,
+    "to" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "txHash" TEXT NOT NULL,
+    "txData" JSONB NOT NULL,
+    "blockNumber" BIGINT NOT NULL,
+    "gasUsed" BIGINT NOT NULL,
+
+    CONSTRAINT "ScoutProjectContractTransaction_pkey" PRIMARY KEY ("txHash")
+);
+
+-- CreateTable
+CREATE TABLE "ScoutProjectContractLog" (
+    "contractId" UUID NOT NULL,
+    "from" TEXT NOT NULL,
+    "txHash" TEXT NOT NULL,
+    "logIndex" INTEGER NOT NULL,
+    "blockNumber" BIGINT NOT NULL,
+
+    CONSTRAINT "ScoutProjectContractLog_pkey" PRIMARY KEY ("txHash")
+);
+
+-- CreateIndex
+CREATE INDEX "ScoutProjectContractTransaction_txHash_idx" ON "ScoutProjectContractTransaction"("txHash");
+
+-- CreateIndex
+CREATE INDEX "ScoutProjectContractTransaction_blockNumber_idx" ON "ScoutProjectContractTransaction"("blockNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ScoutProjectContractTransaction_contractId_txHash_key" ON "ScoutProjectContractTransaction"("contractId", "txHash");
+
+-- CreateIndex
+CREATE INDEX "ScoutProjectContractLog_txHash_idx" ON "ScoutProjectContractLog"("txHash");
+
+-- CreateIndex
+CREATE INDEX "ScoutProjectContractLog_blockNumber_idx" ON "ScoutProjectContractLog"("blockNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ScoutProjectContractLog_contractId_txHash_logIndex_key" ON "ScoutProjectContractLog"("contractId", "txHash", "logIndex");
+
+-- CreateIndex
+CREATE INDEX "ScoutProject_path_idx" ON "ScoutProject"("path");
+
+-- AddForeignKey
+ALTER TABLE "ScoutProjectContractTransaction" ADD CONSTRAINT "ScoutProjectContractTransaction_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "ScoutProjectContract"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ScoutProjectContractLog" ADD CONSTRAINT "ScoutProjectContractLog_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "ScoutProjectContract"("id") ON DELETE CASCADE ON UPDATE CASCADE;
