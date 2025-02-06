b0f6866324bfa5127bee148988cfb02679d16a21
Safwan Shaheer <devorein00@gmail.com>
2025-02-07 00:19:15 +0600

    Amount to string

--- src/prisma/migrations/20250206181905_partner_reward_payout/migration.sql
+++ src/prisma/migrations/20250206181905_partner_reward_payout/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "PartnerRewardPayout" (
     "id" UUID NOT NULL,
     "userId" UUID NOT NULL,
-    "amount" DECIMAL(30,18) NOT NULL,
+    "amount" TEXT NOT NULL,
     "claimedAt" TIMESTAMP(3),
     "txHash" TEXT,
     "payoutContractId" UUID NOT NULL,
