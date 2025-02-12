3c43331ee78b75da2ce8fa18c59d2536b5d8838c
motechFR <mo@anyahq.co>
2025-02-12 15:55:41 +0100

    Add a table for onchain events (#439)
    * Add a tab;e for onchain events
    
    * ci: version bump to 0.111.2-rc-synced-attestat.0
    
    * Rename table
    
    * Regenerate migration with fixes
    
    * ci: version bump to 0.111.3-rc-synced-attestat.0
    
    * Change enum value
    
    * ci: version bump to 0.111.3-rc-synced-attestat.1
    
    * Regenerate migration, dont use uuid in the id column
    
    * ci: version bump to 0.111.3-rc-synced-attestat.2
    
    * ci: version bump to 0.112.1-rc-synced-attestat.0
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20250207131910_builder_status_events/migration.sql
@@ -0,0 +1,29 @@
+-- CreateEnum
+CREATE TYPE "BuilderStatusEventType" AS ENUM ('registered', 'banned', 'unbanned');
+
+-- AlterEnum
+ALTER TYPE "ReferralPlatform" ADD VALUE 'onchain_cron';
+
+-- AlterTable
+ALTER TABLE "BuilderStrike" ADD COLUMN     "onchainStrikeAttestationUid" TEXT,
+ADD COLUMN     "onchainStrikeChainId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "BuilderStatusEvent" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "attestationUid" TEXT NOT NULL,
+    "chainId" INTEGER NOT NULL,
+    "status" "BuilderStatusEventType" NOT NULL,
+    "builderId" UUID NOT NULL,
+
+    CONSTRAINT "BuilderStatusEvent_pkey" PRIMARY KEY ("attestationUid")
+);
+
+-- CreateIndex
+CREATE INDEX "BuilderStatusEvent_builderId_idx" ON "BuilderStatusEvent"("builderId");
+
+-- CreateIndex
+CREATE INDEX "BuilderStatusEvent_createdAt_idx" ON "BuilderStatusEvent"("createdAt");
+
+-- AddForeignKey
+ALTER TABLE "BuilderStatusEvent" ADD CONSTRAINT "BuilderStatusEvent_builderId_fkey" FOREIGN KEY ("builderId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
