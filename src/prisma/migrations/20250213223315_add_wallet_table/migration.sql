b08a06c7a384d118bf8d5b3a7cfb602b09e8b8f8
mattcasey <mattwad@gmail.com>
2025-02-13 15:33:21 -0700

    add log

--- src/prisma/migrations/20250213223315_add_wallet_table/migration.sql
+++ src/prisma/migrations/20250213223315_add_wallet_table/migration.sql
@@ -29,6 +29,15 @@ CREATE TABLE "ScoutProjectWalletTransaction" (
     "gasCost" BIGINT NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "ScoutProjectWalletPollEvent" (
+    "walletId" UUID NOT NULL,
+    "fromBlockNumber" BIGINT NOT NULL,
+    "toBlockNumber" BIGINT NOT NULL,
+    "processedAt" TIMESTAMP(3) NOT NULL,
+    "processTime" INTEGER NOT NULL
+);
+
 -- CreateIndex
 CREATE INDEX "ScoutProjectWallet_projectId_idx" ON "ScoutProjectWallet"("projectId");
 
@@ -53,8 +62,23 @@ CREATE INDEX "ScoutProjectWalletTransaction_createdAt_idx" ON "ScoutProjectWalle
 -- CreateIndex
 CREATE UNIQUE INDEX "ScoutProjectWalletTransaction_walletId_txHash_key" ON "ScoutProjectWalletTransaction"("walletId", "txHash");
 
+-- CreateIndex
+CREATE INDEX "ScoutProjectWalletPollEvent_walletId_idx" ON "ScoutProjectWalletPollEvent"("walletId");
+
+-- CreateIndex
+CREATE INDEX "ScoutProjectWalletPollEvent_toBlockNumber_idx" ON "ScoutProjectWalletPollEvent"("toBlockNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ScoutProjectWalletPollEvent_walletId_toBlockNumber_key" ON "ScoutProjectWalletPollEvent"("walletId", "toBlockNumber");
+
 -- AddForeignKey
 ALTER TABLE "ScoutProjectWallet" ADD CONSTRAINT "ScoutProjectWallet_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "ScoutProject"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ScoutProjectWalletTransaction" ADD CONSTRAINT "ScoutProjectWalletTransaction_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "ScoutProjectWallet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ScoutProjectContractPollEvent" ADD CONSTRAINT "ScoutProjectContractPollEvent_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "ScoutProjectContract"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ScoutProjectWalletPollEvent" ADD CONSTRAINT "ScoutProjectWalletPollEvent_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "ScoutProjectWallet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
