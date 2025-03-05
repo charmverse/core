8f16a86aa4ed0610c642df2309e3d9c68bc0e484
Safwan Shaheer <devorein00@gmail.com>
2025-03-05 19:13:09 +0600

    Scout notification records

---
+++ src/prisma/migrations/20250305131253_scout_notification/migration.sql
@@ -0,0 +1,31 @@
+-- CreateEnum
+CREATE TYPE "ScoutNotificationChannel" AS ENUM ('farcaster', 'email');
+
+-- CreateTable
+CREATE TABLE "ScoutFarcasterNotification" (
+    "id" UUID NOT NULL,
+    "fid" INTEGER NOT NULL,
+    "userId" UUID NOT NULL,
+    "notificationType" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ScoutFarcasterNotification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ScoutEmailNotification" (
+    "id" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "templateVariables" JSONB NOT NULL,
+    "userId" UUID NOT NULL,
+    "notificationType" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ScoutEmailNotification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ScoutFarcasterNotification" ADD CONSTRAINT "ScoutFarcasterNotification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ScoutEmailNotification" ADD CONSTRAINT "ScoutEmailNotification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
