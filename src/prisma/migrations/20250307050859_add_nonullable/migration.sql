2ad0df36942ab5a933b4e495ee216019a5d01749
mattcasey <mattwad@gmail.com>
2025-03-07 01:02:39 -0700

    fast followers

--- src/prisma/migrations/20250307050859_add_nonullable/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `chainType` on table `ScoutProjectWallet` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "ScoutProjectWallet" ALTER COLUMN "chainType" SET NOT NULL;
