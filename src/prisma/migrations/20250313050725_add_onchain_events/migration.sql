fc76ad5e014919d2e66a57741d38bd1da11c23b8
mattcasey <mattwad@gmail.com>
2025-03-12 23:07:32 -0600

    single builder event

--- src/prisma/migrations/20250313050725_add_onchain_events/migration.sql
+++ src/prisma/migrations/20250313050725_add_onchain_events/migration.sql
@@ -2,16 +2,7 @@
 CREATE TYPE "OnchainActivityTier" AS ENUM ('bronze', 'silver', 'gold');
 
 -- AlterEnum
--- This migration adds more than one value to an enum.
--- With PostgreSQL versions 11 and earlier, this is not possible
--- in a single migration. This can be worked around by creating
--- multiple migrations, each migration adding only one value to
--- the enum.
-
-
-ALTER TYPE "BuilderEventType" ADD VALUE 'onchain_activity_bronze';
-ALTER TYPE "BuilderEventType" ADD VALUE 'onchain_activity_silver';
-ALTER TYPE "BuilderEventType" ADD VALUE 'onchain_activity_gold';
+ALTER TYPE "BuilderEventType" ADD VALUE 'onchain_activity';
 
 -- AlterTable
 ALTER TABLE "BuilderEvent" ADD COLUMN     "onchainActivityEventId" UUID;
