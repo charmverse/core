d18e7f9b7d8eb1992d1ed6b1ab8832e03ba5f400
Matt Casey <mattwad@gmail.com>
2025-03-13 15:32:17 -0600

    add models for smart gems (#451)
    * add models for smart gems
    
    * single builder event
    
    * ci: version bump to 0.117.2-rc-contract-gems.0
    
    * rename model
    
    * ci: version bump to 0.117.2-rc-contract-gems.1
    
    * rename enum
    
    * ci: version bump to 0.117.2-rc-contract-gems.2
    
    * update enum for gems
    
    * ci: version bump to 0.117.2-rc-contract-gems.3
    
    * redo migration
    
    * ci: version bump to 0.117.2-rc-contract-gems.4
    
    * update gem value
    
    * ci: version bump to 0.117.2-rc-contract-gems.5
    
    * Revert "update gem value"
    
    This reverts commit f038483d663c0e173c4688da56d70274dd44a287.
    
    * ci: version bump to 0.117.2-rc-contract-gems.6
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20250313195537_add_onchain_achievement/migration.sql
@@ -0,0 +1,37 @@
+-- CreateEnum
+CREATE TYPE "OnchainAchievementTier" AS ENUM ('bronze', 'silver', 'gold');
+
+-- AlterEnum
+ALTER TYPE "BuilderEventType" ADD VALUE 'onchain_achievement';
+
+-- AlterEnum
+ALTER TYPE "GemsReceiptType" ADD VALUE 'onchain_achievement';
+
+-- AlterTable
+ALTER TABLE "BuilderEvent" ADD COLUMN     "onchainAchievementId" UUID;
+
+-- CreateTable
+CREATE TABLE "ScoutProjectOnchainAchievement" (
+    "id" UUID NOT NULL,
+    "projectId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "tier" "OnchainAchievementTier" NOT NULL,
+    "week" TEXT NOT NULL,
+
+    CONSTRAINT "ScoutProjectOnchainAchievement_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "ScoutProjectOnchainAchievement_projectId_idx" ON "ScoutProjectOnchainAchievement"("projectId");
+
+-- CreateIndex
+CREATE INDEX "ScoutProjectOnchainAchievement_week_idx" ON "ScoutProjectOnchainAchievement"("week");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ScoutProjectOnchainAchievement_projectId_week_tier_key" ON "ScoutProjectOnchainAchievement"("projectId", "week", "tier");
+
+-- AddForeignKey
+ALTER TABLE "BuilderEvent" ADD CONSTRAINT "BuilderEvent_onchainAchievementId_fkey" FOREIGN KEY ("onchainAchievementId") REFERENCES "ScoutProjectOnchainAchievement"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ScoutProjectOnchainAchievement" ADD CONSTRAINT "ScoutProjectOnchainAchievement_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "ScoutProject"("id") ON DELETE CASCADE ON UPDATE CASCADE;
