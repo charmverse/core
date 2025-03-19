dddaf6eb3d5d681082b4bfe53748f05f8fc51ced
Safwan Shaheer <devorein00@gmail.com>
2025-03-19 16:59:48 +0600

    Scout app notifications

---
+++ src/prisma/migrations/20250319105935_scout_app_notifications/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "ScoutAppNotification" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "notificationType" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "meta" JSONB NOT NULL,
+
+    CONSTRAINT "ScoutAppNotification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ScoutAppNotification" ADD CONSTRAINT "ScoutAppNotification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
