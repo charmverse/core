36d85a2bef3d0b8a6aa83e4c0e11c4c5bade470e
Safwan Shaheer <devorein00@gmail.com>
2025-03-21 17:22:41 +0600

    scout wallet sanction

--- src/prisma/migrations/20250320113120_wallet_sanction_status/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "WalletSanctionStatus" (
-    "address" TEXT NOT NULL,
-    "isSanctioned" BOOLEAN NOT NULL DEFAULT false,
-    "checkedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "WalletSanctionStatus_pkey" PRIMARY KEY ("address")
-);
