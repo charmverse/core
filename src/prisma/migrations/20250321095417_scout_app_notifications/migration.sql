5e9c37709bb9946dcc0bd11abd63d1c5c0250446
Safwan Shaheer <devorein00@gmail.com>
2025-03-21 15:54:26 +0600

    added indexes

--- src/prisma/migrations/20250321095417_scout_app_notifications/migration.sql
+++ src/prisma/migrations/20250321095417_scout_app_notifications/migration.sql
@@ -10,5 +10,17 @@ CREATE TABLE "ScoutAppNotification" (
     CONSTRAINT "ScoutAppNotification_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE INDEX "ScoutAppNotification_userId_idx" ON "ScoutAppNotification"("userId");
+
+-- CreateIndex
+CREATE INDEX "ScoutAppNotification_read_idx" ON "ScoutAppNotification"("read");
+
+-- CreateIndex
+CREATE INDEX "ScoutEmailNotification_userId_idx" ON "ScoutEmailNotification"("userId");
+
+-- CreateIndex
+CREATE INDEX "ScoutFarcasterNotification_userId_idx" ON "ScoutFarcasterNotification"("userId");
+
 -- AddForeignKey
 ALTER TABLE "ScoutAppNotification" ADD CONSTRAINT "ScoutAppNotification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
