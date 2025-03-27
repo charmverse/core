aadea18f4abbe1f132ffb70e50a6f503a96ebbe7
Safwan Shaheer <devorein00@gmail.com>
2025-03-27 17:45:48 +0600

    developer nft listing

---
+++ src/prisma/migrations/20250327114541_developer_nft_listing/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "DeveloperNftListing" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "builderNftId" UUID NOT NULL,
+    "sellerWallet" TEXT NOT NULL,
+    "buyerWallet" TEXT,
+    "price" INTEGER NOT NULL,
+    "priceDevToken" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "signature" TEXT NOT NULL,
+    "hash" TEXT,
+    "completedAt" TIMESTAMP(3),
+    "cancelledAt" TIMESTAMP(3),
+    "order" JSONB,
+
+    CONSTRAINT "DeveloperNftListing_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "DeveloperNftListing_builderNftId_idx" ON "DeveloperNftListing"("builderNftId");
+
+-- CreateIndex
+CREATE INDEX "DeveloperNftListing_sellerWallet_idx" ON "DeveloperNftListing"("sellerWallet");
+
+-- CreateIndex
+CREATE INDEX "DeveloperNftListing_completedAt_idx" ON "DeveloperNftListing"("completedAt");
+
+-- CreateIndex
+CREATE INDEX "DeveloperNftListing_cancelledAt_idx" ON "DeveloperNftListing"("cancelledAt");
+
+-- CreateIndex
+CREATE INDEX "DeveloperNftListing_buyerWallet_idx" ON "DeveloperNftListing"("buyerWallet");
+
+-- AddForeignKey
+ALTER TABLE "DeveloperNftListing" ADD CONSTRAINT "DeveloperNftListing_builderNftId_fkey" FOREIGN KEY ("builderNftId") REFERENCES "BuilderNft"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DeveloperNftListing" ADD CONSTRAINT "DeveloperNftListing_sellerWallet_fkey" FOREIGN KEY ("sellerWallet") REFERENCES "ScoutWallet"("address") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DeveloperNftListing" ADD CONSTRAINT "DeveloperNftListing_buyerWallet_fkey" FOREIGN KEY ("buyerWallet") REFERENCES "ScoutWallet"("address") ON DELETE SET NULL ON UPDATE CASCADE;
