1f8e2f3662fb667ce0ccd8ea71baa9fca3a1884a
mattcasey <mattwad@gmail.com>
2025-04-02 01:28:10 -0600

    update schema

--- src/prisma/migrations/20250402072804_add_matchup/migration.sql
+++ src/prisma/migrations/20250402072804_add_matchup/migration.sql
@@ -17,7 +17,7 @@ CREATE TABLE "ScoutMatchup" (
 -- CreateTable
 CREATE TABLE "ScoutMatchupSelection" (
     "id" UUID NOT NULL,
-    "matchUpId" UUID NOT NULL,
+    "matchupId" UUID NOT NULL,
     "developerId" UUID NOT NULL,
     "gemsEarned" INTEGER NOT NULL DEFAULT 0,
     "creditsValue" INTEGER NOT NULL DEFAULT 0,
@@ -35,19 +35,19 @@ CREATE INDEX "ScoutMatchup_week_submittedAt_idx" ON "ScoutMatchup"("week", "subm
 CREATE UNIQUE INDEX "ScoutMatchup_createdBy_week_key" ON "ScoutMatchup"("createdBy", "week");
 
 -- CreateIndex
-CREATE INDEX "ScoutMatchupSelection_matchUpId_idx" ON "ScoutMatchupSelection"("matchUpId");
+CREATE INDEX "ScoutMatchupSelection_matchupId_idx" ON "ScoutMatchupSelection"("matchupId");
 
 -- CreateIndex
 CREATE INDEX "ScoutMatchupSelection_developerId_idx" ON "ScoutMatchupSelection"("developerId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ScoutMatchupSelection_matchUpId_developerId_key" ON "ScoutMatchupSelection"("matchUpId", "developerId");
+CREATE UNIQUE INDEX "ScoutMatchupSelection_matchupId_developerId_key" ON "ScoutMatchupSelection"("matchupId", "developerId");
 
 -- AddForeignKey
 ALTER TABLE "ScoutMatchup" ADD CONSTRAINT "ScoutMatchup_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ScoutMatchupSelection" ADD CONSTRAINT "ScoutMatchupSelection_matchUpId_fkey" FOREIGN KEY ("matchUpId") REFERENCES "ScoutMatchup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ScoutMatchupSelection" ADD CONSTRAINT "ScoutMatchupSelection_matchupId_fkey" FOREIGN KEY ("matchupId") REFERENCES "ScoutMatchup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ScoutMatchupSelection" ADD CONSTRAINT "ScoutMatchupSelection_developerId_fkey" FOREIGN KEY ("developerId") REFERENCES "Scout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
