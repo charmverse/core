b006a9b99de10ef62409565828f98d4396a9a61e
Safwan Shaheer <devorein00@gmail.com>
2025-04-04 08:09:46 +0600

    Airdrop claim model

---
+++ src/prisma/migrations/20250404020910_airdrop_claim/migration.sql
@@ -0,0 +1,47 @@
+-- AlterTable
+ALTER TABLE "PartnerRewardPayout" ADD COLUMN     "airdropClaimId" UUID;
+
+-- CreateTable
+CREATE TABLE "AirdropClaim" (
+    "id" UUID NOT NULL,
+    "season" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "contractAddress" TEXT NOT NULL,
+    "chainId" INTEGER NOT NULL,
+    "deployTxHash" TEXT NOT NULL,
+    "ipfsCid" TEXT NOT NULL,
+    "merkleTreeJson" JSONB NOT NULL,
+
+    CONSTRAINT "AirdropClaim_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AirdropClaimPayout" (
+    "id" UUID NOT NULL,
+    "airdropClaimId" UUID NOT NULL,
+    "amount" TEXT NOT NULL,
+    "donationAmount" TEXT NOT NULL,
+    "txHash" TEXT NOT NULL,
+    "walletAddress" TEXT NOT NULL,
+    "claimedAt" TIMESTAMP(3),
+
+    CONSTRAINT "AirdropClaimPayout_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "AirdropClaimPayout_airdropClaimId_idx" ON "AirdropClaimPayout"("airdropClaimId");
+
+-- CreateIndex
+CREATE INDEX "AirdropClaimPayout_txHash_idx" ON "AirdropClaimPayout"("txHash");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AirdropClaimPayout_airdropClaimId_walletAddress_key" ON "AirdropClaimPayout"("airdropClaimId", "walletAddress");
+
+-- AddForeignKey
+ALTER TABLE "PartnerRewardPayout" ADD CONSTRAINT "PartnerRewardPayout_airdropClaimId_fkey" FOREIGN KEY ("airdropClaimId") REFERENCES "AirdropClaim"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AirdropClaimPayout" ADD CONSTRAINT "AirdropClaimPayout_airdropClaimId_fkey" FOREIGN KEY ("airdropClaimId") REFERENCES "AirdropClaim"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AirdropClaimPayout" ADD CONSTRAINT "AirdropClaimPayout_walletAddress_fkey" FOREIGN KEY ("walletAddress") REFERENCES "ScoutWallet"("address") ON DELETE CASCADE ON UPDATE CASCADE;
