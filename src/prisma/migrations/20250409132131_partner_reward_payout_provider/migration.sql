895789c5567c429e51cc4cc9e9aa094ddefa90bd
Safwan Shaheer <devorein00@gmail.com>
2025-04-09 20:14:21 +0600

    Added blocknumber

--- src/prisma/migrations/20250409132131_partner_reward_payout_provider/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "PartnerRewardPayoutContractProvider" AS ENUM ('thirdweb', 'sablier');
-
--- AlterTable
-ALTER TABLE "PartnerRewardPayoutContract" ADD COLUMN     "provider" "PartnerRewardPayoutContractProvider" NOT NULL DEFAULT 'sablier';
