ad3e635bf2797bfb7d9308d414e2784d6640845c
mattcasey <mattwad@gmail.com>
2025-04-28 01:37:08 -0600

    update models

--- src/prisma/migrations/20250428073653_add_matchup_tx/migration.sql
+++ src/prisma/migrations/20250428073653_add_matchup_tx/migration.sql
@@ -5,16 +5,16 @@
 
 */
 -- AlterTable
-ALTER TABLE "ScoutMatchup" ADD COLUMN     "registrationTxId" UUID;
+ALTER TABLE "ScoutMatchup" ADD COLUMN     "decentRegistrationTxId" UUID,
+ADD COLUMN     "registrationTxId" UUID;
 
 -- CreateTable
 CREATE TABLE "BlockchainTransaction" (
     "id" UUID NOT NULL,
-    "txHash" TEXT,
-    "chainId" INTEGER,
-    "decentTxHash" TEXT,
-    "decentChainId" INTEGER,
+    "txHash" TEXT NOT NULL,
+    "chainId" INTEGER NOT NULL,
     "status" "DecentTxStatus" NOT NULL,
+    "error" JSONB,
 
     CONSTRAINT "BlockchainTransaction_pkey" PRIMARY KEY ("id")
 );
@@ -23,13 +23,13 @@ CREATE TABLE "BlockchainTransaction" (
 CREATE INDEX "BlockchainTransaction_txHash_idx" ON "BlockchainTransaction"("txHash");
 
 -- CreateIndex
-CREATE INDEX "BlockchainTransaction_chainId_idx" ON "BlockchainTransaction"("chainId");
-
--- CreateIndex
 CREATE UNIQUE INDEX "BlockchainTransaction_txHash_chainId_key" ON "BlockchainTransaction"("txHash", "chainId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ScoutMatchup_registrationTxId_key" ON "ScoutMatchup"("registrationTxId");
+
+-- AddForeignKey
+ALTER TABLE "ScoutMatchup" ADD CONSTRAINT "ScoutMatchup_decentRegistrationTxId_fkey" FOREIGN KEY ("decentRegistrationTxId") REFERENCES "BlockchainTransaction"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ScoutMatchup" ADD CONSTRAINT "ScoutMatchup_registrationTxId_fkey" FOREIGN KEY ("registrationTxId") REFERENCES "BlockchainTransaction"("id") ON DELETE SET NULL ON UPDATE CASCADE;
