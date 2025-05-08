6a565011e3223d020dd4a700d4766ae9d133deed
Safwan Shaheer <devorein00@gmail.com>
2025-05-08 20:11:45 +0600

    improved space subscription

--- src/prisma/migrations/20250508141121_space_subscription/migration.sql
+++ src/prisma/migrations/20250508141121_space_subscription/migration.sql
@@ -1,10 +1,32 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "SubscriptionTier" ADD VALUE 'bronze';
+ALTER TYPE "SubscriptionTier" ADD VALUE 'silver';
+ALTER TYPE "SubscriptionTier" ADD VALUE 'gold';
+ALTER TYPE "SubscriptionTier" ADD VALUE 'grants';
+
+-- AlterTable
+ALTER TABLE "Space" ADD COLUMN     "subscriptionBalance" TEXT;
+
 -- CreateTable
 CREATE TABLE "SpaceSubscription" (
     "id" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "tier" "SubscriptionTier" NOT NULL,
     "spaceId" UUID NOT NULL,
-    "balance" TEXT NOT NULL,
-    "txHash" TEXT NOT NULL,
+    "decentStatus" "DecentTxStatus" NOT NULL,
+    "decentError" JSONB,
+    "decentTxHash" TEXT,
+    "sourceChainId" INTEGER,
+    "txHash" TEXT,
+    "chainId" INTEGER,
+    "decentPayload" JSONB NOT NULL,
     "cancelledAt" TIMESTAMP(3),
 
     CONSTRAINT "SpaceSubscription_pkey" PRIMARY KEY ("id")
