70b4489a5722ebf40cfe41adc2188ad83f3d5341
Safwan Shaheer <devorein00@gmail.com>
2025-05-16 23:06:33 +0600

    cancelled subscription tier (#469)
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/prisma/migrations/20250516115839_tier_change_event/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Space" ADD COLUMN     "subscriptionCancelledAt" TIMESTAMP(3),
+ADD COLUMN     "subscriptionCancelledBy" UUID;
+
+-- CreateTable
+CREATE TABLE "SpaceSubscriptionTierChangeEvent" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "spaceId" UUID NOT NULL,
+    "previousTier" "SpaceSubscriptionTier" NOT NULL,
+    "newTier" "SpaceSubscriptionTier" NOT NULL,
+
+    CONSTRAINT "SpaceSubscriptionTierChangeEvent_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "SpaceSubscriptionTierChangeEvent" ADD CONSTRAINT "SpaceSubscriptionTierChangeEvent_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
