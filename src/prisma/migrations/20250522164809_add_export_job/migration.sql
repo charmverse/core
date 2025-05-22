7d746be21784b10d2235d80510dc5c41a7c19600
mattcasey <mattwad@gmail.com>
2025-05-22 10:48:18 -0600

    updaet schema

--- src/prisma/migrations/20250522164809_add_export_job/migration.sql
+++ src/prisma/migrations/20250522164809_add_export_job/migration.sql
@@ -5,6 +5,7 @@ CREATE TYPE "SpaceExportJobStatus" AS ENUM ('pending', 'completed', 'failed');
 CREATE TABLE "SpaceExportJob" (
     "id" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" UUID NOT NULL,
     "spaceId" UUID NOT NULL,
     "status" "SpaceExportJobStatus" NOT NULL,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -13,6 +14,15 @@ CREATE TABLE "SpaceExportJob" (
 
     CONSTRAINT "SpaceExportJob_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE INDEX "SpaceExportJob_spaceId_status_idx" ON "SpaceExportJob"("spaceId", "status");
+
+-- CreateIndex
+CREATE INDEX "SpaceExportJob_createdBy_idx" ON "SpaceExportJob"("createdBy");
+
+-- AddForeignKey
+ALTER TABLE "SpaceExportJob" ADD CONSTRAINT "SpaceExportJob_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "SpaceExportJob" ADD CONSTRAINT "SpaceExportJob_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
