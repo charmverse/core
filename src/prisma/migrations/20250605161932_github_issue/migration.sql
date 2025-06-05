d6bd6d715fd1f335180f5cb20aaea7fb8eccdc3f
Safwan Shaheer <devorein00@gmail.com>
2025-06-05 22:20:02 +0600

    Improved github issue model

--- src/prisma/migrations/20250605161932_github_issue/migration.sql
+++ src/prisma/migrations/20250605161932_github_issue/migration.sql
@@ -1,14 +1,11 @@
 -- CreateTable
 CREATE TABLE "GithubIssue" (
-    "id" UUID NOT NULL,
-    "pullRequestId" TEXT NOT NULL,
+    "pullRequestNumber" INTEGER NOT NULL,
     "githubEventId" UUID NOT NULL,
     "repoId" INTEGER NOT NULL,
     "issueNumber" INTEGER NOT NULL,
-    "tags" JSONB NOT NULL DEFAULT '[]',
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "GithubIssue_pkey" PRIMARY KEY ("id")
+    "tags" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateIndex
@@ -18,7 +15,7 @@ CREATE INDEX "GithubIssue_repoId_idx" ON "GithubIssue"("repoId");
 CREATE INDEX "GithubIssue_githubEventId_idx" ON "GithubIssue"("githubEventId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "GithubIssue_pullRequestId_repoId_key" ON "GithubIssue"("pullRequestId", "repoId");
+CREATE UNIQUE INDEX "GithubIssue_issueNumber_repoId_key" ON "GithubIssue"("issueNumber", "repoId");
 
 -- AddForeignKey
 ALTER TABLE "GithubIssue" ADD CONSTRAINT "GithubIssue_githubEventId_fkey" FOREIGN KEY ("githubEventId") REFERENCES "GithubEvent"("id") ON DELETE CASCADE ON UPDATE CASCADE;
