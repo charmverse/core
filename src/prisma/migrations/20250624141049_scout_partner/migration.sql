bf3e7f13150c42dd48e3dd82e6d324a64e72b314
Safwan Shaheer <devorein00@gmail.com>
2025-06-24 08:11:06 -0600

    Added scout partner id to github repo

--- src/prisma/migrations/20250624141049_scout_partner/migration.sql
+++ src/prisma/migrations/20250624141049_scout_partner/migration.sql
@@ -4,6 +4,9 @@ CREATE TYPE "ScoutPartnerStatus" AS ENUM ('active', 'paused', 'completed');
 -- AlterTable
 ALTER TABLE "BuilderEvent" ADD COLUMN     "scoutPartnerId" TEXT;
 
+-- AlterTable
+ALTER TABLE "GithubRepo" ADD COLUMN     "scoutPartnerId" TEXT;
+
 -- CreateTable
 CREATE TABLE "ScoutPartner" (
     "id" TEXT NOT NULL,
@@ -20,3 +23,6 @@ CREATE TABLE "ScoutPartner" (
 
 -- AddForeignKey
 ALTER TABLE "BuilderEvent" ADD CONSTRAINT "BuilderEvent_scoutPartnerId_fkey" FOREIGN KEY ("scoutPartnerId") REFERENCES "ScoutPartner"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GithubRepo" ADD CONSTRAINT "GithubRepo_scoutPartnerId_fkey" FOREIGN KEY ("scoutPartnerId") REFERENCES "ScoutPartner"("id") ON DELETE SET NULL ON UPDATE CASCADE;
